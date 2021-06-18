2769ccc8cf4739456feb1ae1e1436ff0cb4ad723
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2021-06-18 14:46:11 +0200

    added friction visualisation

--- src/analyse_mountaincar_env.py
+++ src/analyse_mountaincar_env.py
@@ -14,11 +14,14 @@ x_min = -1.2
 x_max = 0.6
 m = 1
 g = 1
+mu_r = 1  # friction coefficients
 X = np.linspace(x_min, x_max, 100)
 
 Y = height(X)
-F_gs = - np.sin(np.arctan(1.35*np.cos(3*X))) * m * g
+alpha = np.arctan(1.35*np.cos(3*X))
+F_gs = - np.sin(alpha) * m * g
 F_gs_gym = - np.cos(3*X) * m * g
+F_rs = - mu_r * np.cos(alpha) * m * g
 
 fig = plt.figure(figsize=(6, 8))
 axes = fig.subplots(nrows=2, ncols=1, sharex=True)
@@ -29,11 +32,11 @@ ax.set_ylabel("y")
 ax.set_title("Height")
 
 ax = axes[1]
-data = pd.DataFrame([X, F_gs, F_gs_gym]).T
-data.columns = ["x", "analytical", "gym approximation"]
+data = pd.DataFrame([X, F_gs, F_gs_gym, F_rs]).T
+data.columns = ["x", "$F_{gs}$ (analytical)", "$F_{gs}$ (gym approximation)", "$F_{rs}$ (analytical)"]
 data = data.melt(id_vars=["x"])
-data = data.rename(columns={"variable": "source"})
-ax = sns.lineplot(data=data, x="x", y="value", hue="source", ax=ax)
+data = data.rename(columns={"variable": "force"})
+ax = sns.lineplot(data=data, x="x", y="value", hue="force", ax=ax)
 ax.set_xlabel("x")
 ax.set_ylabel("F_gs")
 ax.set_title("Gravity force in direction of trajectory s")
