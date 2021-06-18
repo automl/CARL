5c103e64c4798b726a8d6652791baec110be67dc
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2021-06-18 12:29:28 +0200

    added custom mountaincarenv and made reset state intervals configurable/explicit

--- src/classic_control/meta_mountaincar.py
+++ src/classic_control/meta_mountaincar.py
@@ -15,14 +15,37 @@ DEFAULT_CONTEXT = {
     "goal_velocity": 0,  # unit?
     "force": 0.001,  # unit?
     "gravity": 0.0025,  # unit?
+    "min_position_start": -0.6,
+    "max_position_start": -0.4,
+    "min_velocity_start": 0.,
+    "max_velocity_start": 0.,
 }
 
 
-class MountainCarEnv(MetaEnv):
+class CustomMountainCarEnv(gccenvs.mountain_car.MountainCarEnv):
+    def __init__(self, goal_velocity: float = 0.):
+        super(CustomMountainCarEnv, self).__init__(goal_velocity=goal_velocity)
+        self.min_position_start = -0.6
+        self.max_position_start = -0.4
+        self.min_velocity_start = 0.
+        self.max_velocity_start = 0.
+        
+    def reset_state(self):
+        return np.array([
+            self.np_random.uniform(low=self.min_position_start, high=self.max_position_start),  # sample start position
+            self.np_random.uniform(low=self.min_velocity_start, high=self.max_velocity_start)   # sample start velocity
+        ])
+
+    def reset(self):
+        self.state = self.reset_state()
+        return np.array(self.state)
+
+
+class MetaMountainCarEnv(MetaEnv):
     def __init__(
             self,
-            env: gym.Env = gccenvs.mountain_car.MountainCarEnv,
-            contexts: List[Dict] = [],
+            env: gym.Env = CustomMountainCarEnv(),
+            contexts: Dict[int, Dict] = {},  # ??? what should be the type of the dict keys?
             instance_mode: str = "rr"
     ):
         """
@@ -36,12 +59,12 @@ class MountainCarEnv(MetaEnv):
         instance_mode: str, optional
         """
         if not contexts:
-            contexts = [DEFAULT_CONTEXT]
+            contexts = {0: DEFAULT_CONTEXT}
         super().__init__(env=env, contexts=contexts, instance_mode=instance_mode)
 
         self._update_context()
-        self.viewer = None
-        self.seed()
+        # self.viewer = None
+        # self.seed()
 
     def _update_context(self):
         self.min_position = self.context["min_position"]
@@ -49,6 +72,10 @@ class MountainCarEnv(MetaEnv):
         self.max_speed = self.context["max_speed"]
         self.goal_position = self.context["goal_position"]
         self.goal_velocity = self.context["goal_velocity"]
+        self.min_position_start = self.context["min_position_start"]
+        self.max_position_start = self.context["max_position_start"]
+        self.min_velocity_start = self.context["min_velocity_start"]
+        self.max_velocity_start = self.context["max_velocity_start"]
         self.force = self.context["force"]
         self.gravity = self.context["gravity"]
 
@@ -63,110 +90,3 @@ class MountainCarEnv(MetaEnv):
         self.observation_space = spaces.Box(
             self.low, self.high, dtype=np.float32
         )
-
-    def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
-
-
-    def step(self, action):
-        assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
-
-        position, velocity = self.state
-        velocity += (action - 1) * self.force + math.cos(3 * position) * (-self.gravity)
-        velocity = np.clip(velocity, -self.max_speed, self.max_speed)
-        position += velocity
-        position = np.clip(position, self.min_position, self.max_position)
-        if (position == self.min_position and velocity < 0):
-            velocity = 0
-
-        done = bool(
-            position >= self.goal_position and velocity >= self.goal_velocity
-        )
-        reward = -1.0
-
-        self.state = (position, velocity)
-        return np.array(self.state), reward, done, {}
-
-
-    def reset(self):
-        self.state = np.array([self.np_random.uniform(low=-0.6, high=-0.4), 0])
-        return np.array(self.state)
-
-
-    def _height(self, xs):
-        return np.sin(3 * xs) * .45 + .55
-
-
-    def render(self, mode='human'):
-        screen_width = 600
-        screen_height = 400
-
-        world_width = self.max_position - self.min_position
-        scale = screen_width / world_width
-        carwidth = 40
-        carheight = 20
-
-        if self.viewer is None:
-            from gym.envs.classic_control import rendering
-            self.viewer = rendering.Viewer(screen_width, screen_height)
-            xs = np.linspace(self.min_position, self.max_position, 100)
-            ys = self._height(xs)
-            xys = list(zip((xs - self.min_position) * scale, ys * scale))
-
-            self.track = rendering.make_polyline(xys)
-            self.track.set_linewidth(4)
-            self.viewer.add_geom(self.track)
-
-            clearance = 10
-
-            l, r, t, b = -carwidth / 2, carwidth / 2, carheight, 0
-            car = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
-            car.add_attr(rendering.Transform(translation=(0, clearance)))
-            self.cartrans = rendering.Transform()
-            car.add_attr(self.cartrans)
-            self.viewer.add_geom(car)
-            frontwheel = rendering.make_circle(carheight / 2.5)
-            frontwheel.set_color(.5, .5, .5)
-            frontwheel.add_attr(
-                rendering.Transform(translation=(carwidth / 4, clearance))
-            )
-            frontwheel.add_attr(self.cartrans)
-            self.viewer.add_geom(frontwheel)
-            backwheel = rendering.make_circle(carheight / 2.5)
-            backwheel.add_attr(
-                rendering.Transform(translation=(-carwidth / 4, clearance))
-            )
-            backwheel.add_attr(self.cartrans)
-            backwheel.set_color(.5, .5, .5)
-            self.viewer.add_geom(backwheel)
-            flagx = (self.goal_position - self.min_position) * scale
-            flagy1 = self._height(self.goal_position) * scale
-            flagy2 = flagy1 + 50
-            flagpole = rendering.Line((flagx, flagy1), (flagx, flagy2))
-            self.viewer.add_geom(flagpole)
-            flag = rendering.FilledPolygon(
-                [(flagx, flagy2), (flagx, flagy2 - 10), (flagx + 25, flagy2 - 5)]
-            )
-            flag.set_color(.8, .8, 0)
-            self.viewer.add_geom(flag)
-
-        pos = self.state[0]
-        self.cartrans.set_translation(
-            (pos - self.min_position) * scale, self._height(pos) * scale
-        )
-        self.cartrans.set_rotation(math.cos(3 * pos))
-
-        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
-
-
-    def get_keys_to_action(self):
-        # Control with left and right arrow keys.
-        return {(): 1, (276,): 0, (275,): 2, (275, 276): 1}
-
-
-    def close(self):
-        if self.viewer:
-            self.viewer.close()
-            self.viewer = None
-
