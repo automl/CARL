274070707bd71147837847517c68b17be0af6df4
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2021-06-18 12:28:27 +0200

    retrieve proper function (not from env but from MetaEnv)

--- src/meta_env.py
+++ src/meta_env.py
@@ -11,9 +11,16 @@ class MetaEnv(Wrapper):
         self.context_index = 0
 
     def reset(self):
-        self._progess_instance()
+        self._progress_instance()
         self._update_context()
         return self.env.reset()
+
+    def __getattr__(self, name):
+        if name in ["_progress_instance", "_update_context"]:
+            return getattr(self, name)
+        if name.startswith('_'):
+            raise AttributeError("attempted to get missing private attribute '{}'".format(name))
+        return getattr(self.env, name)
 
     def _progress_instance(self):
         if self.instance_mode == "random":
