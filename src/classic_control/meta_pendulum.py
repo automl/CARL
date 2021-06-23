93b2fe08748b82ef85c99eaeb7cc3a7369346a88
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2021-06-23 16:19:57 +0200

    added context bounds and properly assigning observation space.

--- src/classic_control/meta_pendulum.py
+++ src/classic_control/meta_pendulum.py
@@ -9,11 +9,18 @@ from src.meta_env import MetaEnv
 
 DEFAULT_CONTEXT = {
     "max_speed": 8.,
-    "max_torque": 2.,
     "dt":  0.05,
     "g": 10.0,
     "m": 1.,
     "l": 1.,
+}
+
+CONTEXT_BOUNDS = {
+    "max_speed": (-np.inf, np.inf),  # TODO: discuss limits
+    "dt": (0, np.inf),
+    "g": (0, np.inf),
+    "m": (0, np.inf),
+    "l": (0, np.inf),
 }
 
 
@@ -21,12 +28,24 @@ class MetaPendulumEnv(MetaEnv):
     def __init__(
             self,
             env: gym.Env = gccenvs.pendulum.PendulumEnv(),
-            contexts: Dict[int, Dict] = {},  # ??? what should be the type of the dict keys?
+            contexts: Dict[str, Dict] = {},
             instance_mode: str = "rr",
             hide_context: bool = False,
             add_gaussian_noise_to_context: bool = True,
             gaussian_noise_std_percentage: float = 0.01
     ):
+        """
+        Max torque is not a context feature because it changes the action space.
+
+        Parameters
+        ----------
+        env
+        contexts
+        instance_mode
+        hide_context
+        add_gaussian_noise_to_context
+        gaussian_noise_std_percentage
+        """
         if not contexts:
             contexts = {0: DEFAULT_CONTEXT}
         super().__init__(
@@ -42,20 +61,15 @@ class MetaPendulumEnv(MetaEnv):
 
     def _update_context(self):
         self.env.max_speed = self.context["max_speed"]
-        self.env.max_torque = self.context["max_torque"]  # TODO: allow to change action space? NO!
         self.env.dt = self.context["dt"]
         self.env.l = self.context["l"]
         self.env.m = self.context["m"]
         self.env.g = self.context["g"]
 
         high = np.array([1., 1., self.max_speed], dtype=np.float32)
-        self.action_space = spaces.Box(
-            low=-self.max_torque,
-            high=self.max_torque, shape=(1,),
-            dtype=np.float32
-        )
-        self.observation_space = spaces.Box(
+        self.env.observation_space = spaces.Box(
             low=-high,
             high=high,
             dtype=np.float32
         )
+        self.observation_space = self.env.observation_space
