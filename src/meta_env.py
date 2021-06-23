5bd396139665ff131ebf4c11ce0149f1f573338f
Theresa Eimer <eimer@tnt.uni-hannover.de>
2021-06-23 09:48:50 +0200

    hide context option

--- src/meta_env.py
+++ src/meta_env.py
@@ -8,12 +8,14 @@ class MetaEnv(Wrapper):
             env,
             contexts,
             instance_mode="rr",
+            hide_context=False,
             add_gaussian_noise_to_context: bool = True,
             gaussian_noise_std_percentage: float = 0.01
     ):
         super().__init__(env=env)
         self.contexts = contexts
         self.instance_mode = instance_mode
+        self.hide_context = hide_context
         self.context = contexts[0]
         self.context_index = 0
 
@@ -24,7 +26,16 @@ class MetaEnv(Wrapper):
     def reset(self):
         self._progress_instance()
         self._update_context()
-        return self.env.reset()
+        state = self.env.reset()
+        if not hide_context:
+            state = np.concatenate(state, np.array(list(self.context.values())))
+        return state
+
+    def step(self, action):
+        state, reward, done, info = self.env.step(action)
+        if not hide_context:
+            state = np.concatenate(state, np.array(list(self.context.values())))
+        return state, reward, done, info
 
     def __getattr__(self, name):
         if name in ["_progress_instance", "_update_context"]:
