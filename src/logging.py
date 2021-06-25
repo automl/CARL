c92a07c75d582e0e8bda106fdf47349c31050fdc
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2021-06-25 09:05:50 +0200

    added logger(+config). logging to tensorboard, progress csv and context history.

--- src/logging.py
+++ src/logging.py
@@ -1,13 +1,104 @@
 """
 after each step:
-    reward
-    step
+    reward (progress.csv)
+    step (progress.csv)
 
 after each episode:
-    context
-    episode
+    context (context_history.csv)
+    episode (context_history.csv)
+    step (context_history.csv)
 
 once on train start:
-    experiment config (env, agent, set of contexts)
+    experiment config (env, agent, seed, set of contexts)
     hyperparameters
+
+
+stablebaselines tensorboard logger logs:
+    - rollout/ep_len_mean
+    - rollout/ep_rew_mean
+    - time/fps
+    - train/approx_kl
+    - train/clip_fraction
+    - train/clip_range
+    - train/entropy_loss
+    - train/explained_variance
+    - train/learning_rate
+    - train/loss
+    - train/policy_gradient_loss
+    - train/value_loss
 """
+from typing import Union, Optional
+from pathlib import Path
+import argparse
+import configargparse
+import pandas as pd
+import numpy as np
+
+from stable_baselines3.common.logger import configure
+
+
+class TrialLogger(object):
+    def __init__(
+            self,
+            logdir: Union[str, Path],
+            parser: configargparse.ArgParser,
+            trial_setup_args: Optional[argparse.Namespace] = None,
+    ):
+        self.parser = parser
+        seed = trial_setup_args.seed
+        agent = trial_setup_args.agent
+        self.logdir = Path(logdir) / f"{agent}_{seed}"
+        self.logdir.mkdir(parents=True, exist_ok=True)
+
+        self.trial_setup_args = trial_setup_args
+        self.trial_setup_fn = self.logdir / "trial_setup.ini"
+
+        self.context_history_fn = self.logdir / "context_history.csv"
+        self.prepared_context_history_file = False
+
+        configure(str(self.logdir), ["stdout", "csv", "tensorboard"])
+
+    def write_trial_setup(self):
+        output_file_paths = [str(self.trial_setup_fn)]
+        self.parser.write_config_file(parsed_namespace=self.trial_setup_args, output_file_paths=output_file_paths)
+
+    def write_context(self, episode: int, step: int, context: dict):
+        """
+        Context will be written to csv file.
+
+        The format is as follows:
+            episode,step,context_feature_0,context_feature_1,...,context_feature_n
+            0,1,345345,234234,...,234234
+
+        Parameters
+        ----------
+        episode: int
+        step: int
+        context: dict
+
+        Returns
+        -------
+        None
+        """
+        columns = ["episode", "step"] + list(context.keys())
+        values = [episode, step] + list(context.values())
+        df = pd.DataFrame(values).T
+        df.columns = columns
+
+        write_header = False
+        mode = "a"
+        if not self.prepared_context_history_file:
+            write_header = True
+            mode = "w"
+            self.prepared_context_history_file = True
+
+        df.to_csv(
+            path_or_buf=self.context_history_fn,
+            sep=",",
+            header=write_header,
+            index=False,
+            mode=mode
+        )
+
+
+
