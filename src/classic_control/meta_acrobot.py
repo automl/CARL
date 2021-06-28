c4b5ac4f0d431aa1c950c6a675b524209c30c61b
Theresa Eimer <eimer@tnt.uni-hannover.de>
2021-06-28 11:55:02 +0200

    Cartpole & Acrobot defaults, limits + interface update

--- src/classic_control/meta_acrobot.py
+++ src/classic_control/meta_acrobot.py
@@ -1,8 +1,55 @@
+import numpy as np
+from gym.envs.classic_control import AcrobotEnv
 from src.meta_env import MetaEnv
 
-class MetaCartPoleEnv(MetaEnv):
-    def __init__(self, env, contexts, instance_mode):
-        super().__init__(env, contexts, instance_mode)
+DEFAULT_CONTEXT = {
+    "link_length_1": 1,  # should be seen as 100% default and scaled
+    "link_length_2": 1,  # should be seen as 100% default and scaled
+    "link_mass_1": 1,  # should be seen as 100% default and scaled
+    "link_mass_2": 1,  # should be seen as 100% default and scaled
+    "link_com_1": 0.5,  # Percentage of the length of link one
+    "link_com_2": 0.5,  # Percentage of the length of link one
+    "link_moi": 1, # should be seen as 100% default and scaled
+    "max_velocity_1": 4*np.pi,
+    "max_velocity_2": 9*np.pi,
+}
+
+CONTEXT_BOUNDS = {
+    "link_length_1": (0.1, 10), # Links can be shrunken and grown by a factor of 10
+    "link_length_2": (0.1, 10),
+    "link_mass_1": (0.1, 10),  # Link mass can be shrunken and grown by a factor of 10
+    "link_mass_2": (0.1, 10),
+    "link_com_1": (0, 1),  # Center of mass can move from one end to the other
+    "link_com_2": (0, 1),
+    "link_moi": (0.1, 10), # Moments on inertia can be shrunken and grown by a factor of 10
+    "max_velocity_1": (0.4*np.pi, 40*np.pi), # Velocity can vary by a factor of 10 in either direction
+    "max_velocity_2": (0.9*np.pi, 90*np.pi),
+}
+
+class MetaAcrobotEnv(MetaEnv):
+    def __init__(
+            self,
+            env: gym.Env = AcrobotEnv,
+            contexts: Dict[str, Dict] = {},
+            instance_mode: str = "rr",
+            hide_context: bool = False,
+            add_gaussian_noise_to_context: bool = True,
+            gaussian_noise_std_percentage: float = 0.01,
+            logger: Optional[TrialLogger] = None,
+    ):
+        if not contexts:
+            contexts = {0: DEFAULT_CONTEXT}
+        super().__init__(
+            env=env,
+            contexts=contexts,
+            instance_mode=instance_mode,
+            hide_context=hide_context,
+            add_gaussian_noise_to_context=add_gaussian_noise_to_context,
+            gaussian_noise_std_percentage=gaussian_noise_std_percentage,
+            logger=logger
+        )
+        self.whitelist_gaussian_noise = list(DEFAULT_CONTEXT.keys())  # allow to augment all values
+        self._update_context()
 
     def _update_context(self):
         self.env.LINK_LENGTH_1 = self.context["link_length_1"]
@@ -11,7 +58,7 @@ class MetaCartPoleEnv(MetaEnv):
         self.env.LINK_MASS_2 = self.context["link_mass_2"]
         self.env.LINK_COM_POS_1 = self.context["link_com_1"]
         self.env.LINK_COM_POS_2 = self.context["link_com_2"]
+        self.env.LINK_MOI = self.context["link_moi"]
         self.env.MAX_VEL_1 = self.context["max_velocity_1"]
         self.env.MAX_VEL_2 = self.context["max_velocity_2"]
-        self.env.book_or_nips = self.context["dynamics"]
 
--- src/classic_control/meta_cartpole.py
+++ src/classic_control/meta_cartpole.py
@@ -1,23 +1,35 @@
+from gym.envs.classic_control import CartPoleEnv
 from src.meta_env import MetaEnv
 
 DEFAULT_CONTEXT = {
     "gravity": 9.8,
-    "masscart": 1.,
-    "masspole":  0.1,
-    "pole_length": 0.5,
+    "masscart": 1., # Should be seen as 100% and scaled accordingly
+    "masspole":  0.1, # Should be seen as 100% and scaled accordingly
+    "pole_length": 0.5, # Should be seen as 100% and scaled accordingly
     "force_magnifier": 10.,
-    "update_interval": 0.02,
-    "kinematics": 'euler'
+    "update_interval": 0.02, # Seconds between updates
+}
+
+CONTEXT_BOUNDS = {
+    "gravity": (0.1, np.inf), # Positive gravity
+    "masscart": (0.1, 10), # Cart mass can be varied by a factor of 10
+    "masspole":  (0.01, 1), # Pole mass can be varied by a factor of 10
+    "pole_length": (0.05, 5), # Pole length can be varied by a factor of 10
+    "force_magnifier": (1, 100), # Force magnifier can be varied by a factor of 10
+    "update_interval": (0.002, 0.2), # Update interval can be varied by a factor of 10
 }
 
 class MetaCartPoleEnv(MetaEnv):
-    def __init__(self,
-            env: gym.Env = gccenvs.pendulum.PendulumEnv(),
-            contexts: Dict[int, Dict] = {},  # ??? what should be the type of the dict keys?
+    def __init__(
+            self,
+            env: gym.Env = CartPoleEnv,
+            contexts: Dict[str, Dict] = {},
             instance_mode: str = "rr",
             hide_context: bool = False,
             add_gaussian_noise_to_context: bool = True,
-            gaussian_noise_std_percentage: float = 0.01):
+            gaussian_noise_std_percentage: float = 0.01,
+            logger: Optional[TrialLogger] = None,
+    ):
         if not contexts:
             contexts = {0: DEFAULT_CONTEXT}
         super().__init__(
@@ -27,8 +39,10 @@ class MetaCartPoleEnv(MetaEnv):
             hide_context=hide_context,
             add_gaussian_noise_to_context=add_gaussian_noise_to_context,
             gaussian_noise_std_percentage=gaussian_noise_std_percentage,
+            logger=logger
         )
         self.whitelist_gaussian_noise = list(DEFAULT_CONTEXT.keys())  # allow to augment all values
+        self._update_context()
 
     def _update_context(self):
         self.env.gravity = self.context["gravity"]
@@ -37,5 +51,4 @@ class MetaCartPoleEnv(MetaEnv):
         self.env.length = self.context["pole_length"]
         self.env.force_mag = self.context["force_magnifier"]
         self.env.tau = self.context["update_interval"]
-        self.env.kinematics_integrator = self.context["kinematics"]
 
