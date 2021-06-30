63ee62dc32bf9ee4d1c9bfc64c46fdce8b1da721
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2021-06-30 16:26:37 +0200

    registered metamountaincarcontinuous

--- src/classic_control/__init__.py
+++ src/classic_control/__init__.py
@@ -2,13 +2,20 @@ from src.classic_control.meta_mountaincar import MetaMountainCarEnv
 from src.classic_control.meta_pendulum import MetaPendulumEnv
 from src.classic_control.meta_acrobot import MetaAcrobotEnv
 from src.classic_control.meta_cartpole import MetaCartPoleEnv
+from src.classic_control.meta_mountaincarcontinuous import MetaMountainCarContinuousEnv
 
 # Contexts and bounds by name
 from src.classic_control.meta_acrobot import DEFAULT_CONTEXT as MetaAcrobotEnv_defaults
 from src.classic_control.meta_acrobot import CONTEXT_BOUNDS as MetaAcrobotEnv_bounds
+
 from src.classic_control.meta_cartpole import DEFAULT_CONTEXT as MetaCartPoleEnv_defaults
 from src.classic_control.meta_cartpole import CONTEXT_BOUNDS as MetaCartPoleEnv_bounds
+
 from src.classic_control.meta_pendulum import DEFAULT_CONTEXT as MetaPendulumEnv_defaults
 from src.classic_control.meta_pendulum import CONTEXT_BOUNDS as MetaPendulumEnv_bounds
+
 from src.classic_control.meta_mountaincar import DEFAULT_CONTEXT as MetaMountainCarEnv_defaults
 from src.classic_control.meta_mountaincar import CONTEXT_BOUNDS as MetaMountainCarEnv_bounds
+
+from src.classic_control.meta_mountaincarcontinuous import DEFAULT_CONTEXT as MetaMountainCarContinuousEnv_defaults
+from src.classic_control.meta_mountaincarcontinuous import CONTEXT_BOUNDS as MetaMountainCarContinuousEnv_bounds
