bc1596bded64934714ae138398f05c2f25fb71d7
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2021-07-09 09:38:46 +0200

    made import of learna conditional (issues a warning if not found)

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -1,6 +1,14 @@
 from src.envs.classic_control import *
 from src.envs.box2d import *
 from src.envs.brax import *
-# TODO: make this conditional
-from src.envs.rna import *
+
+# RNA environment loading
+import importlib
+import warnings
+RNA_spec = importlib.util.find_spec("RNA")
+found = RNA_spec is not None
+if found:
+    from src.envs.rna import *
+else:
+    warnings.warn("Module 'RNA' not found. Please follow installation guide for RNA environment.")
 
