0c9e076c4baf4207242946f396a5afc3a3caa07a
Frederik Schubert <frederik.schubert@mailbox.org>
2021-07-12 12:49:43 +0200

    add meta mario env based on toadgan

--- src/envs/mario/meta_mario_env.py
+++ src/envs/mario/meta_mario_env.py
@@ -1,3 +1,4 @@
+from src.envs.mario.toad_gan import generate_level
 from typing import Dict, Optional
 
 import gym
@@ -5,30 +6,28 @@ import numpy as np
 from src.envs.meta_env import MetaEnv
 from src.trial_logger import TrialLogger
 
+
 from .mario_env import MarioEnv
 
-DEFAULT_CONTEXT = {
-    "level_idx": 0,
-    "width": 200 # TODO(frederik): lookup correct with of level 1-1
-    # TODO(frederik): make height also part of context?
-}
+DEFAULT_CONTEXT = {"level_idx": 0, "width": 200, "height": 16}
 
 CONTEXT_BOUNDS = {
     "level_idx": (0, 14, int),
-    "width": (16, 2000, int)
+    "width": (16, np.inf, int),
+    "height": (8, 32, int),
 }
 
 
 class MetaMarioEnv(MetaEnv):
     def __init__(
-            self,
-            env: gym.Env = MarioEnv(levels=[]), # TODO(frederik): set default levels
-            contexts: Dict[int, Dict] = {},
-            instance_mode: str = "rr",
-            hide_context: bool = False,
-            add_gaussian_noise_to_context: bool = True,
-            gaussian_noise_std_percentage: float = 0.01,
-            logger: Optional[TrialLogger] = None,
+        self,
+        env: gym.Env = MarioEnv(levels=[]),
+        contexts: Dict[int, Dict] = {},
+        instance_mode: str = "rr",
+        hide_context: bool = False,
+        add_gaussian_noise_to_context: bool = True,
+        gaussian_noise_std_percentage: float = 0.01,
+        logger: Optional[TrialLogger] = None,
     ):
         if not contexts:
             contexts = {0: DEFAULT_CONTEXT}
@@ -39,13 +38,14 @@ class MetaMarioEnv(MetaEnv):
             hide_context=hide_context,
             add_gaussian_noise_to_context=add_gaussian_noise_to_context,
             gaussian_noise_std_percentage=gaussian_noise_std_percentage,
-            logger=logger
+            logger=logger,
         )
-        self.whitelist_gaussian_noise = list(DEFAULT_CONTEXT.keys())  # allow to augment all values
+        self.whitelist_gaussian_noise = list(
+            DEFAULT_CONTEXT.keys()
+        )  # allow to augment all values
         self._update_context()
 
     def _update_context(self):
-        # TODO(frederik): generate levels according to context using TOAD-GAN
-        # TODO(frederik): filter generated levels by reachability?
-        # TODO(frederik): update levels in env
-
+        # TODO(frederik): filter generated levels by reachability
+        level = generate_level(self.context["width"], self.context["height"], self.context["level_index"])
+        self.env.levels = [level]
---
+++ src/envs/mario/toad_gan.py
@@ -0,0 +1,75 @@
+from dataclasses import dataclass
+import functools
+import os
+from src.envs.mario.generate_sample import generate_sample
+
+import torch
+
+
+@dataclass
+class TOADGAN:
+    def __init__(self, Gs, Zs, reals, NoiseAmp, token_list, num_layers):
+        self.generators = Gs
+        self.noise_maps = Zs
+        self.reals = reals
+        self.noise_amplitudes = NoiseAmp
+        self.token_list = token_list
+        self.num_layer = num_layers
+
+    @property
+    def original_height(self):
+        return self.reals[-1].shape[-2]
+
+    @property
+    def original_width(self):
+        return self.reals[-1].shape[-1]
+
+
+GENERATOR_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "TOAD-GUI", "generators", "v2")
+)
+GENERATOR_PATHS = sorted(
+    os.listdir(GENERATOR_DIR),
+    key=lambda name: [int(index) for index in name.replace("TOAD_GAN_", "").split("-")],
+)
+
+
+@functools.lru_cache(maxsize=None)
+def load_generator(level_index: int):
+    gen_path = os.path.join(GENERATOR_DIR, GENERATOR_PATHS[level_index])
+    reals = torch.load(
+        "%s/reals.pth" % gen_path,
+        map_location="cuda:0" if torch.cuda.is_available() else "cpu",
+    )
+    Zs = torch.load(
+        "%s/noise_maps.pth" % gen_path,
+        map_location="cuda:0" if torch.cuda.is_available() else "cpu",
+    )
+    NoiseAmp = torch.load(
+        "%s/noise_amplitudes.pth" % gen_path,
+        map_location="cuda:0" if torch.cuda.is_available() else "cpu",
+    )
+    token_list = torch.load("%s/token_list.pth" % gen_path)
+    num_layers = torch.load("%s/num_layer.pth" % gen_path)
+    Gs = torch.load(
+        "%s/generators.pth" % gen_path,
+        map_location="cuda:0" if torch.cuda.is_available() else "cpu",
+    )
+    return TOADGAN(
+        Gs=Gs,
+        Zs=Zs,
+        reals=reals,
+        NoiseAmp=NoiseAmp,
+        num_layers=num_layers,
+        token_list=token_list,
+    )
+
+
+def generate_level(width: int, height: int, level_index: int):
+    toad_gan = load_generator(level_index)
+    level = generate_sample(
+        **vars(toad_gan),
+        scale_h=width / toad_gan.original_width,
+        scale_v=height / toad_gan.original_height
+    )
+    return level
