53f7a18ecff600d15ed798df97b5ad7d4eae1658
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2021-08-06 14:43:40 +0200

    included array

--- src/slurm_file_builder.py
+++ src/slurm_file_builder.py
@@ -11,18 +11,23 @@ hide_context = False
 print(env)
 hide_context_dir_str = "_hidecontext" if hide_context else ""
 hide_context_cmd_str = "--hide_context" if hide_context else ""
+eval_freq = 10000 if "Racing" in env else 1000
 
 mail_user = "benjamin@tnt.uni-hannover.de"
 output_filename = "slurmout/slurm-%j.out"
 time = "48:00:00"
 mem_per_cpu = "2000M"
-basecommand = "python run_stablebaselines.py --num_contexts 100 --steps 1000000 --add_context_feature_names_to_logdir"
+basecommand = "python run_stablebaselines.py --num_contexts 100 --steps 1000000 --add_context_feature_names_to_logdir --hp_file hyperparameter.yml"
 cpus_per_task = "1"
 
 env_defaults = getattr(envs, f"{env}_defaults")
 outdir = f"results/singlecontextfeature_{default_sample_std_percentage}{hide_context_dir_str}/box2d/{env}"
-basecommand += f" --outdir {outdir}  --num_workers {cpus_per_task} --default_sample_std_percentage {default_sample_std_percentage} {hide_context_cmd_str}"
+basecommand += f" --outdir {outdir}  --num_workers {cpus_per_task} --default_sample_std_percentage {default_sample_std_percentage}"
+basecommand += f" {hide_context_cmd_str} --eval_freq {eval_freq} --seed $SLURM_ARRAY_TASK_ID " \
+               f"--scale_context_features by_default"
 context_features = list(env_defaults.keys())
+seeds = [0, 1, 2, 3, 4]
+array = f"{min(seeds)}-{max(seeds)}"
 
 sbuilder = SlurmBuilder(
     job_name=job_name,
@@ -31,13 +36,9 @@ sbuilder = SlurmBuilder(
     time=time,
     cpus_per_task=cpus_per_task,
     mem_per_cpu=mem_per_cpu,
+    array=array,
     output_filename=output_filename,
     iteration_list=[
-        {
-            "name": "seed",
-            "id": "s",
-            "values": [0, 1, 2, 3, 4],
-        },
         {
             "name": "env",
             "id": "env",
