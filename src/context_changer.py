43c5a6a8897f86ec52a8819b9ef2f78838c0f269
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2021-08-10 14:53:55 +0200

    added doc and todo

--- src/context_changer.py
+++ src/context_changer.py
@@ -1,9 +1,37 @@
 import numpy as np
+from typing import Union, List
 
 
+# TODO rename this module, clearly distinguish between context changer, context sampler and context utils
 def add_gaussian_noise(
-        default_value: float, percentage_std: float = 0.01, random_generator: np.random.Generator = None):
-    std = percentage_std * np.abs(default_value)
+        default_value: Union[float, List[float]],
+        percentage_std: float = 0.01,
+        random_generator: np.random.Generator = None
+) -> Union[float, List[float]]:
+    """
+    Add gaussian noise to default value.
+
+    Parameters
+    ----------
+    default_value: Union[float, List[float]]
+        Mean of normal distribution. Can be a scalar or a list of floats. If it is a list(-like) with length n, the
+        output will also be of length n.
+    percentage_std: float, optional = 0.01
+        Relative standard deviation, multiplied with default value (mean) is standard deviation of normal distribution.
+        If the default value is 0, percentage_std is assumed to be the absolute standard deviation.
+    random_generator: np.random.Generator, optional = None
+        Optional random generator to ensure deterministic behavior.
+
+    Returns
+    -------
+    Union[float, List[float]]
+        Default value with gaussian noise. If input was list (or array) with length n, output is also list (or array)
+        with length n.
+    """
+    if default_value != 0:
+        std = percentage_std * np.abs(default_value)
+    else:
+        std = percentage_std
     mean = np.zeros_like(default_value)
     if not random_generator:
         random_generator = np.random.default_rng()
@@ -17,6 +45,7 @@ if __name__ == "__main__":
     seed = 123456
     rng = np.random.default_rng(seed=seed)
     default_value = 10
+    default_value = list(np.arange(0, 4))
     percentage_std = 0.01
     n_samples = 1000
     values = np.array([add_gaussian_noise(
