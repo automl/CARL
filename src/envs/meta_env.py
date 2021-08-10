39ecab8d6f940f20e63c41144212eff548179204
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2021-08-10 16:25:37 +0200

    added doc + reordering of init

--- src/envs/meta_env.py
+++ src/envs/meta_env.py
@@ -2,21 +2,30 @@ import gym
 from gym import Wrapper
 from gym import spaces
 import numpy as np
-from typing import Dict, Tuple, Union, List, Optional
+from typing import Dict, Tuple, Union, List, Optional, Any
 from src.context_changer import add_gaussian_noise
 from src.context_utils import get_context_bounds
 from src.trial_logger import TrialLogger
 
 
 class MetaEnv(Wrapper):
+    """
+    Meta-environment formulating the original environments as cMDPs.
+
+    Here, a context feature can be anything defining the behavior of the
+    environment. An instance is the environment with a specific context.
+
+    Can change the context after each episode.
+    """
     available_scale_methods = ["by_default", "by_mean", "no"]
+    available_instance_modes = ["random", "rr", "roundrobin"]
 
     def __init__(
             self,
-            env,
-            contexts,
-            instance_mode="rr",
-            hide_context=False,
+            env: gym.Env,
+            contexts: Dict[Any, Dict[Any, Any]],
+            instance_mode: str = "rr",
+            hide_context: bool = False,
             add_gaussian_noise_to_context: bool = False,
             gaussian_noise_std_percentage: float = 0.01,
             logger: Optional[TrialLogger] = None,
@@ -24,14 +33,66 @@ class MetaEnv(Wrapper):
             scale_context_features: str = "no",
             default_context: Optional[Dict] = None,
     ):
+        """
+
+        Parameters
+        ----------
+        env: gym.Env
+            Environment which context features are made visible / which is turned into a cMDP.
+        contexts: Dict[Any, Dict[Any, Any]]
+            Dict of contexts/instances. Key are context id, values are contexts as
+            Dict[context feature id, context feature value].
+        instance_mode: str, default="rr"
+            Instance sampling mode. Available modes are 'random' or 'rr'/'roundrobin'.
+        hide_context: bool = False
+            If False, the context will be appended to the original environment's state.
+        add_gaussian_noise_to_context: bool = False
+            Wether to add Gaussian noise to the context with the relative standard deviation
+            'gaussian_noise_std_percentage'.
+        gaussian_noise_std_percentage: float = 0.01
+            The relative standard deviation for the Gaussian noise. The actual standard deviation
+            is calculated by 'gaussian_noise_std_percentage' * context feature value.
+        logger: TrialLogger, optional
+            Optional TrialLogger which takes care of setting up logging directories and handles
+            custom logging.
+        max_episode_length: int = 1e6
+            Maximum length of episode in (time)steps. Cutoff.
+        scale_context_features: str = "no"
+            Wether to scale context features. Available modes are 'no', 'by_mean' and 'by_default'.
+            'by_mean' scales the context features by their mean over all passed instances and
+            'by_default' scales the context features by their default values ('default_context').
+        default_context: Dict
+            The default context of the environment. Used for scaling the context features if applicable.
+
+        Raises
+        ------
+        ValueError
+            If the choice of instance_mode is not available.
+        ValueError
+            If the choice of scale_context_features is not available.
+
+        """
         super().__init__(env=env)
+        # Gather args
         self.contexts = contexts
+        if instance_mode not in self.available_instance_modes:
+            raise ValueError(f"instance_mode '{instance_mode}' not in '{self.available_instance_modes}'.")
         self.instance_mode = instance_mode
         self.hide_context = hide_context
-        self.context_index = 0
+        self.cutoff = max_episode_length
+        self.logger = logger
+        self.add_gaussian_noise_to_context = add_gaussian_noise_to_context
+        self.gaussian_noise_std_percentage = gaussian_noise_std_percentage
+
+        self.step_counter = 0  # type: int # increased in/after step
+        self.episode_counter = -1  # type: int # increased during reset
+        self.whitelist_gaussian_noise = None    # type: Optional[List[str]] # holds names of context features
+                                                # where it is allowed to add gaussian noise
+
+        # Set initial context
+        self.context_index = 0  # type: int
         context_keys = list(contexts.keys())
         self.context = contexts[context_keys[self.context_index]]
-        self.cutoff = max_episode_length
 
         # Scale context features
         if scale_context_features not in self.available_scale_methods:
@@ -51,17 +112,51 @@ class MetaEnv(Wrapper):
 
         self.build_observation_space()
 
-    def reset(self):
+    def reset(self, **kwargs):
+        """
+        Reset environment.
+
+        Parameters
+        ----------
+        kwargs: Dict
+            Any keyword arguments passed to env.reset().
+
+        Returns
+        -------
+        state
+            State of environment after reset.
+
+        """
         self.episode_counter += 1
         self._progress_instance()
         self._update_context()
         self._log_context()
-        state = self.env.reset()
+        state = self.env.reset(**kwargs)
         if not self.hide_context:
+            # Append context to state
             state = np.concatenate((state, np.array(list(self.context.values()))))
         return state
 
-    def step(self, action):
+    def step(self, action) -> (Any, Any, bool, Dict):
+        """
+        Step the environment.
+
+        1. Step
+        2. Add (potentially scaled) context features to state if hide_context = False.
+
+        Emits done if the environment has taken more steps than cutoff (max_episode_length).
+
+        Parameters
+        ----------
+        action:
+            Action to pass to env.step.
+
+        Returns
+        -------
+        state, reward, done, info: Any, Any, bool, Dict
+            Standard signature.
+
+        """
         # Step the environment
         state, reward, done, info = self.env.step(action)
 
@@ -80,20 +175,36 @@ class MetaEnv(Wrapper):
             # Add context features to state
             state = np.concatenate((state, context_feature_values))
 
-        self.step_counter += 1
+        self.step_counter += 1  # TODO do we need to reset the step counter?
         if self.step_counter >= self.cutoff:
             done = True
         return state, reward, done, info
 
     def __getattr__(self, name):
         # TODO: does this work with activated noise? I think we need to update it
-        if name in ["_progress_instance", "_update_context"]:
+        # We need this because our MetaEnv has underscore class methods which would
+        # through an error otherwise
+        if name in ["_progress_instance", "_update_context", "_log_context"]:
             return getattr(self, name)
         if name.startswith('_'):
             raise AttributeError("attempted to get missing private attribute '{}'".format(name))
         return getattr(self.env, name)
 
     def _progress_instance(self):
+        """
+        Progress instance.
+
+        In this case instance is a specific context.
+        1. Select instance with the instance_mode. If the instance_mode is random, randomly select
+        the next instance from the set of contexts. If instance_mode is rr or roundrobin, select
+        the next instance.
+        2. If Gaussian noise should be added to whitelisted context features, do so.
+
+        Returns
+        -------
+        None
+
+        """
         if self.instance_mode == "random":
             # TODO pass seed?
             self.context_index = np.random.choice(np.arange(len(self.contexts.keys())))
@@ -126,7 +237,37 @@ class MetaEnv(Wrapper):
             env_upper_bounds: Optional[Union[List, np.array]] = None,
             context_bounds: Optional[Dict[str, Tuple[float]]] = None
     ):
-        if not isinstance(self.observation_space, spaces.Box):
+        """
+        Build observation space of environment.
+
+        If the hide_context = False, add correct bounds for the context features to the
+        observation space.
+
+        Parameters
+        ----------
+        env_lower_bounds: Optional[Union[List, np.array]], default=None
+            Lower bounds for environment observation space. If env_lower_bounds and env_upper_bounds
+            both are None, (re-)create bounds (low=-inf, high=inf) with correct dimension.
+        env_upper_bounds: Optional[Union[List, np.array]], default=None
+            Upper bounds for environment observation space.
+        context_bounds: Optional[Dict[str, Tuple[float]]], default=None
+            Lower and upper bounds for context features.
+            The bounds are provided as a Dict containing the context feature names/ids as keys and the
+            bounds per feature as a tuple (low, high).
+            If None and the context should not be hidden,
+            creates default bounds with (low=-inf, high=inf) with correct dimension.
+
+        Raises
+        ------
+        ValueError:
+            If (env.)observation space is not gym.spaces.Box and the context should not be hidden (hide_context = False).
+
+        Returns
+        -------
+        None
+
+        """
+        if not isinstance(self.observation_space, spaces.Box) and not self.hide_context:
             raise ValueError("This environment does not yet support non-hidden contexts. Only supports "
                              "Box observation spaces.")
 
@@ -157,9 +298,25 @@ class MetaEnv(Wrapper):
         self.observation_space = self.env.observation_space  # make sure it is the same object
 
     def _update_context(self):
+        """
+        Update the context feature values of the environment.
+
+        Returns
+        -------
+        None
+
+        """
         raise NotImplementedError
 
     def _log_context(self):
+        """
+        Log context.
+
+        Returns
+        -------
+        None
+
+        """
         if self.logger:
             self.logger.write_context(self.episode_counter, self.step_counter, self.context)
 
