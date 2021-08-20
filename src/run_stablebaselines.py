22030f82ee352cb58bb7b308ebfc16bcad4db776
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2021-08-20 08:53:01 +0200

    removed unnecessary function

--- src/run_stablebaselines.py
+++ src/run_stablebaselines.py
@@ -28,24 +28,6 @@ from src.context_sampler import sample_contexts
 from utils.hyperparameter_processing import preprocess_hyperparams
 
 
-# TODO: what does this do? Do we even need it?
-def make_env(env_id, rank, seed=0):
-    """
-    Utility function for multiprocessed env.
-
-    :param env_id: (str) the environment ID
-    :param num_env: (int) the number of environments you wish to have in subprocesses
-    :param seed: (int) the inital seed for RNG
-    :param rank: (int) index of the subprocess
-    """
-    def _init():
-        env = gym.make(env_id)
-        env.seed(seed + rank)
-        return env
-    set_random_seed(seed)
-    return _init
-
-
 def get_parser() -> configargparse.ArgumentParser:
     """
     Creates new argument parser for running baselines.
