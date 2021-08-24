78e4ac40c924628c3e33426ef49fa863e392896a
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2021-08-24 11:51:23 +0200

    plot height

--- src/test_mountaincar.py
+++ src/test_mountaincar.py
@@ -1,12 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def height(xs):
+    return np.sin(3 * xs) * .45 + .55
+
+
 x_min = -1.2
+x_min = -2
 x_max = 0.6
 X = np.linspace(x_min, x_max, 100)
 Y = np.cos(3*X)
 Y2 = np.sin(np.arctan(-3*np.sin(3*X)))
+Y3 = height(X)
 
-plt.plot(X, Y)
-plt.plot(X, Y2)
+# plt.plot(X, Y, label="their force(?)")
+# plt.plot(X, Y2, label="my force(?)")
+plt.plot(X, Y3, label="height")
+plt.legend()
 plt.show()
