f05f68087e833bebdcdc8d2300efedacca9b6114
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2021-08-26 07:56:44 +0200

    update (properly read HPs and maybe use normalization)

--- src/run_pb2.py
+++ src/run_pb2.py
@@ -14,23 +14,60 @@ from ray.tune.schedulers.pb2 import PB2
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import VecNormalize
 
 from src.utils.hyperparameter_processing import preprocess_hyperparams
+from src.run_stablebaselines import main, get_parser
+from src.context_sampler import sample_contexts
 
 
-def setup_model(env, hp_file, num_envs, config, checkpoint_dir):
+def setup_model(env, hp_file, num_envs, hide_context, context_feature_args, default_sample_std_percentage, config, checkpoint_dir):
+    # with open(hp_file, "r") as f:
+    #     hyperparams_dict = yaml.safe_load(f)
+    #     hyperparams = hyperparams_dict[env]
+    #     hyperparams, env_wrappers, _, _ = preprocess_hyperparams(hyperparams)
+    #
+    # from src.envs import CARLAnt
+    # EnvCls = partial(eval(env), contexts=None)
+    # env = make_vec_env(EnvCls, n_envs=num_envs, wrapper_class=env_wrappers)
+    # eval_env = make_vec_env(EnvCls, n_envs=1, wrapper_class=env_wrappers)
+
+    # hyperparams = {}
+    # env_wrapper = None
+    # normalize = False
+    # normalize_kwargs = {}
     with open(hp_file, "r") as f:
         hyperparams_dict = yaml.safe_load(f)
-        hyperparams = hyperparams_dict[env]
-        hyperparams, env_wrappers, _, _ = preprocess_hyperparams(hyperparams)
+    hyperparams = hyperparams_dict[env]
+    hyperparams, env_wrapper, normalize, normalize_kwargs = preprocess_hyperparams(hyperparams)
 
+    num_contexts = 100
+    contexts = sample_contexts(
+            env,
+            context_feature_args,
+            num_contexts,
+            default_sample_std_percentage=default_sample_std_percentage
+        )
+    env_logger = None
     from src.envs import CARLAnt
-    EnvCls = partial(eval(env), contexts=None)
-    env = make_vec_env(EnvCls, n_envs=num_envs, wrapper_class=env_wrappers)
-    eval_env = make_vec_env(EnvCls, n_envs=1, wrapper_class=env_wrappers)
+    EnvCls = partial(
+        eval(env),
+        contexts=contexts,
+        logger=env_logger,
+        hide_context=hide_context,
+    )
+    env = make_vec_env(EnvCls, n_envs=num_envs, wrapper_class=env_wrapper)
+    eval_env = make_vec_env(EnvCls, n_envs=1, wrapper_class=env_wrapper)
+    if normalize:
+        env = VecNormalize(env, **normalize_kwargs)
+        eval_normalize_kwargs = normalize_kwargs.copy()
+        eval_normalize_kwargs["norm_reward"] = False
+        eval_normalize_kwargs["training"] = False
+        eval_env = VecNormalize(eval_env, **eval_normalize_kwargs)
 
     if checkpoint_dir:
         print(checkpoint_dir)
+        checkpoint_dir = str(checkpoint_dir)
         checkpoint = os.path.join(checkpoint_dir, "checkpoint")
         model = PPO.load(checkpoint, env=env)
     else:
@@ -47,12 +84,23 @@ def eval_model(model, eval_env, config):
             action, _ = model.predict(state)
             state, reward, done, _ = eval_env.step(action)
             eval_reward.append(reward)
-    return tune.report(mean_accuracy=np.mean(eval_reward),
-                current_config=config)
+    return tune.report(
+        mean_accuracy=np.mean(eval_reward),
+        current_config=config
+    )
 
 
-def train_ppo(env, hp_file, num_envs, config, checkpoint_dir=None):
-    model, eval_env = setup_model(env, hp_file, num_envs, config, checkpoint_dir)
+def train_ppo(env, hp_file, num_envs, hide_context, context_feature_args, default_sample_std_percentage, config, checkpoint_dir=None):
+    model, eval_env = setup_model(
+        env=env,
+        hp_file=hp_file,
+        num_envs=num_envs,
+        config=config,
+        checkpoint_dir=checkpoint_dir,
+        hide_context=hide_context,
+        context_feature_args=context_feature_args,
+        default_sample_std_percentage=default_sample_std_percentage
+    )
     model.learning_rate = config["learning_rate"]
     model.gamma = config["gamma"]
     model.ent_coef = config["ent_coef"]
@@ -68,21 +116,7 @@ def train_ppo(env, hp_file, num_envs, config, checkpoint_dir=None):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--env", type=str, default="CARLAnt", help="Environment to optimize hyperparameters for")
-    parser.add_argument(
-        "--hp_file", default=os.path.abspath(os.path.join(os.path.dirname(__file__), "hyperparameter.yml")), type=str
-    )
-    parser.add_argument(
-        "--num_envs", type=int, default=os.cpu_count() or 1
-    )
-    parser.add_argument(
-        "--checkpoint_dir", type=str, default="results/experiments/pb2"
-    )
-    parser.add_argument(
-        "--output_dir", type=str, default="results/experiments/pb2/default"
-    )
+    parser = get_parser()
     parser.add_argument(
         "--server-address",
         type=str,
@@ -90,16 +124,27 @@ if __name__ == "__main__":
         required=False,
         help="The address of server to connect to if using "
         "Ray Client.")
-    args, _ = parser.parse_known_args()
-    checkpoint_dir = Path(args.checkpoint_dir)
-    checkpoint_dir.mkdir(parents=True, exist_ok=True)
+    parser.add_argument(
+        "--checkpoint_dir", type=str, default="results/experiments/pb2"
+    )
+
+    args, unknown_args = parser.parse_known_args()
+    args.env = "CARLAnt"
+    args.outdir = os.path.join("results/experiments/pb2", args.env)
+    args.hide_context = True
+    args.default_sample_std_percentage = 0.25
+    args.context_feature_args = ['friction']
+    checkpoint_dir = args.checkpoint_dir
+
+    # checkpoint_dir = Path(checkpoint_dir)
+    # checkpoint_dir.mkdir(parents=True, exist_ok=True)
     # args.num_envs = 1
     if args.server_address:
         ray.util.connect(args.server_address)
     else:
         ray.init()
 
-    print(os.getcwd())
+    print("current workdir:", os.getcwd())
 
     pbt = PB2(
         perturbation_interval=20,
@@ -126,7 +171,15 @@ if __name__ == "__main__":
         }
 
     analysis = tune.run(
-        partial(train_ppo, args.env, args.hp_file, args.num_envs),  #, args.checkpoint_dir),
+        partial(
+            train_ppo,
+            args.env,
+            args.hp_file,
+            args.num_envs,
+            args.hide_context,
+            args.context_feature_args,
+            args.default_sample_std_percentage
+        ),
         name="pb2",
         scheduler=pbt,
         metric="mean_accuracy",
