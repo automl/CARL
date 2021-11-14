5ba6d69e460b4ed9a9f7b649623b614cbd3271d3
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2021-11-14 13:34:52 +0100

    plot parallel

--- src/training/smac_plot_trajectory.py
+++ src/training/smac_plot_trajectory.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from collections import OrderedDict
 import numpy as np
 import warnings
+from pandas.plotting import parallel_coordinates
 
 
 def fill_trajectory(performance_list, time_list, replace_nan=np.NaN):
@@ -47,51 +48,19 @@ def fill_trajectory(performance_list, time_list, replace_nan=np.NaN):
     return performance, time_, melted
 
 
-if __name__ == '__main__':
-    """
-    Assumed folder structure:
-    
-    outdir / smag_logs / run_runid / traj.json
-    outdir / trial_setup.json
-    """
-    outdir = "/home/benjamin/Dokumente/code/tmp/CARL/src/results/optimized/classic_control/CARLPendulumEnv/0.75_contexthidden/g/"
-
-    convert_sec_to_hours = True
-    key_time = "wallclock_time"
-    key_performance = "cost"
-
-    filenames = glob.glob(os.path.join(outdir, "**", "traj.json"), recursive=True)
-
-    data_list = []
+def plot_smac_trajectory(data, key_time, key_performance, key_group):
+    groups = data.groupby(key_group)
     performance_list = []
     time_list = []
-    for fname in filenames:
-        with open(fname, 'r') as file:
-            lines = file.readlines()
-        data = [json.loads(line) for line in lines]
-        data = pd.DataFrame(data)
-
-        info_fn = "trial_setup.json"
-        fname = Path(fname)
-        info_fname = fname.parent.parent.parent / info_fn
-        with open(info_fname, 'r') as file:
-            info = json.load(file)
-
-        agent = info.get("agent", None)
-        seed = info.get("seed", None)
-        data["seed"] = [seed] * len(data)
-        data["agent"] = [agent] * len(data)
-
-        if len(data) > 1:
-            performance_list.append(data[key_performance].to_numpy())
-            time_data = data[key_time].to_numpy()
-            time_data[0] = 0
-            time_list.append(time_data)
-            data_list.append(data)
+    for group_value, group_df in groups:
+        if len(group_df) > 1:
+            performance_list.append(group_df[key_performance].to_numpy())
+            times = group_df[key_time].to_numpy()
+            times[0] = 0
+            time_list.append(times)
         else:
-            warnings.warn(f"Short trajectory (length {len(data)}) found for trial {info_fname}. Do not add to data.")
+            warnings.warn(f"Short trajectory (length {len(group_df)}) found for trial {group_value}. Do not add to data.")
 
-    data = pd.concat(data_list)
     performances, times, data_filled = fill_trajectory(performance_list=performance_list, time_list=time_list)
 
     data_filled.index.name = key_time
@@ -115,4 +84,78 @@ if __name__ == '__main__':
     ax.set_yscale('log')
     fig.set_tight_layout(True)
     plt.show()
+
+
+def gather_smac_data(outdir, key_group="exp_source"):
+    filenames = glob.glob(os.path.join(outdir, "**", "traj.json"), recursive=True)
+
+    data_list = []
+    for fname in filenames:
+        with open(fname, 'r') as file:
+            lines = file.readlines()
+        data = [json.loads(line) for line in lines]
+        data = pd.DataFrame(data)
+
+        info_fn = "trial_setup.json"
+        fname = Path(fname)
+        info_fname = fname.parent.parent.parent / info_fn
+        with open(info_fname, 'r') as file:
+            info = json.load(file)
+
+        agent = info.get("agent", None)
+        seed = info.get("seed", None)
+        data["seed"] = [seed] * len(data)
+        data["agent"] = [agent] * len(data)
+        data[key_group] = [info_fname] * len(data)
+
+        data_list.append(data)
+
+    data = pd.concat(data_list)
+
+    return data
+
+
+if __name__ == '__main__':
+    """
+    Assumed folder structure:
+    
+    outdir / smag_logs / run_runid / traj.json
+    outdir / trial_setup.json
+    """
+    outdir = "/home/benjamin/Dokumente/code/tmp/CARL/src/results/optimized/classic_control/CARLPendulumEnv/0.75_contexthidden/g/"
+
+    convert_sec_to_hours = True
+    key_time = "wallclock_time"
+    key_performance = "cost"
+    key_group = "exp_source"
+
+    data = gather_smac_data(outdir=outdir, key_group=key_group)
+    # plot_smac_trajectory(data=data, key_time=key_time, key_performance=key_performance, key_group=key_group)
+
+    key_hps = 'incumbent'
+
+    data.reset_index(drop=True, inplace=True)
+    df = pd.json_normalize(data['incumbent'])
+    hp_names = list(df.columns)
+    df = pd.concat([data, df], axis=1)
+    # var_name = "hyperparameter"
+    # value_name = "hyperparameter_value"
+    # id_vars = [c for c in df.columns if c not in hp_names]
+    # df = pd.melt(
+    #     df, ignore_index=False, id_vars=id_vars, value_vars=hp_names,
+    #     var_name=var_name, value_name=value_name)
+
+    class_column = 'seed'
+    keep_cols = [class_column] + hp_names
+    plot_data = df[keep_cols]
+    plot_data = plot_data.sort_values(class_column)
+    figsize = (6, 4)
+    dpi = 200
+    fig = plt.figure(figsize=figsize, dpi=dpi)
+    ax = fig.add_subplot(111)
+    ax = parallel_coordinates(frame=plot_data, class_column=class_column, ax=ax)
+    fig.set_tight_layout(True)
+    plt.show()
+
+
 
