99319431c577ebf038c81e91177427f874920ff7
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2021-11-25 16:58:18 +0100

    better parallel coordinates

--- src/training/smac_plot_trajectory.py
+++ src/training/smac_plot_trajectory.py
@@ -9,9 +9,12 @@ from collections import OrderedDict
 import numpy as np
 import warnings
 from pandas.plotting import parallel_coordinates
+from typing import Optional, List
+from matplotlib.path import Path
+import matplotlib.patches as patches
 
 
-def fill_trajectory(performance_list, time_list, replace_nan=np.NaN):
+def fill_trajectory(performance_list, time_list, seed_list: Optional[List[int]] = None, replace_nan=np.NaN):
     """
     https://github.com/automl/plotting_scripts/blob/master/plottingscripts/utils/merge_test_performance_different_times.py
     """
@@ -43,7 +46,19 @@ def fill_trajectory(performance_list, time_list, replace_nan=np.NaN):
                          "\t(d) any other reason.")
 
     melted = pd.melt(merged, ignore_index=False, var_name="list_index", value_name="performance")
+    melted['seed'] = [np.nan] * len(melted)
+    melted['list_index'] = melted['list_index'].apply(int)
+    seeds = melted['seed'].to_numpy()
+    for i, seed in enumerate(seed_list):
+        # print(i)
+        # where = np.where(list_index == i)
+        seeds[melted['list_index'] == i] = seed
+        # print(melted['seed'][melted['list_index'] == i])
+    melted['seed'] = seeds
+
     melted.index.name = "time"
+
+    melted.reset_index(inplace=True)
 
     return performance, time_, melted
 
@@ -52,19 +67,22 @@ def plot_smac_trajectory(data, key_time, key_performance, key_group):
     groups = data.groupby(key_group)
     performance_list = []
     time_list = []
+    seed_list = []
     for group_value, group_df in groups:
         if len(group_df) > 1:
             performance_list.append(group_df[key_performance].to_numpy())
             times = group_df[key_time].to_numpy()
             times[0] = 0
             time_list.append(times)
+            seed_list.append(group_df['seed'].unique()[0])  # just one seed per group
         else:
             warnings.warn(f"Short trajectory (length {len(group_df)}) found for trial {group_value}. Do not add to data.")
 
-    performances, times, data_filled = fill_trajectory(performance_list=performance_list, time_list=time_list)
+    performances, times, data_filled = fill_trajectory(
+        performance_list=performance_list, time_list=time_list, seed_list=seed_list)
 
     data_filled.index.name = key_time
-    data_filled.rename(columns={'list_index': 'seed', 'performance': key_performance}, inplace=True)
+    data_filled.rename(columns={'performance': key_performance}, inplace=True)
     data_filled.reset_index(level=0, inplace=True)
 
     del_ids = data['evaluations'] == 0
@@ -77,6 +95,7 @@ def plot_smac_trajectory(data, key_time, key_performance, key_group):
     key_time = new_time_key
     if convert_sec_to_hours:
         plot_data[key_time] /= 3600
+
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -117,7 +136,7 @@ def gather_smac_data(outdir, key_group="exp_source"):
     return data
 
 
-def plot_parallel_coordinates(data, key_hps='incumbent', class_column='seed'):
+def plot_parallel_coordinates__(data, key_hps='incumbent', class_column='seed'):
     data.reset_index(drop=True, inplace=True)
     df = pd.json_normalize(data[key_hps])
     hp_names = list(df.columns)
@@ -152,6 +171,100 @@ def plot_parallel_coordinates(data, key_hps='incumbent', class_column='seed'):
     plt.show()
 
 
+def plot_parallel_coordinates(
+        data: pd.DataFrame,
+        key_hps: str = 'incumbent',
+        class_column: str = 'seed',
+        draw_straight: bool = True
+):
+    data.reset_index(drop=True, inplace=True)
+    df = pd.json_normalize(data[key_hps])
+    hp_names = list(df.columns)
+    df = pd.concat([data, df], axis=1)
+    # var_name = "hyperparameter"
+    # value_name = "hyperparameter_value"
+    # id_vars = [c for c in df.columns if c not in hp_names]
+    # df = pd.melt(
+    #     df, ignore_index=False, id_vars=id_vars, value_vars=hp_names,
+    #     var_name=var_name, value_name=value_name)
+
+    keep_cols = [class_column] + hp_names
+    plot_data = df[keep_cols]
+    # only plot last incumbent
+    groups = plot_data.groupby(class_column)
+    new_data = []
+    for group_id, group_df in groups:
+        if len(group_df) > 1:
+            smalldf = pd.DataFrame(group_df.iloc[-1]).T
+            new_data.append(smalldf)
+    plot_data = pd.concat(new_data)
+    plot_data = plot_data.sort_values(class_column)
+
+    fig, host = plt.subplots()
+
+    ynames = [c for c in plot_data.columns if c != class_column]
+
+    df = plot_data[ynames]
+    ys = df.values
+    N = ys.shape[0]
+    category = plot_data[class_column].to_numpy(dtype=np.int)
+
+    ymins = ys.min(axis=0)
+    ymaxs = ys.max(axis=0)
+    dys = ymaxs - ymins
+    ymins -= dys * 0.05  # add 5% padding below and above
+    ymaxs += dys * 0.05
+    dys = ymaxs - ymins
+
+    # transform all data to be compatible with the main axis
+    zs = np.zeros_like(ys)
+    zs[:, 0] = ys[:, 0]
+    zs[:, 1:] = (ys[:, 1:] - ymins[1:]) / dys[1:] * dys[0] + ymins[0]
+
+    axes = [host] + [host.twinx() for i in range(ys.shape[1] - 1)]
+    for i, ax in enumerate(axes):
+        ax.set_ylim(ymins[i], ymaxs[i])
+        ax.spines['top'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        if ax != host:
+            ax.spines['left'].set_visible(False)
+            ax.yaxis.set_ticks_position('right')
+            ax.spines["right"].set_position(("axes", i / (ys.shape[1] - 1)))
+
+    host.set_xlim(0, ys.shape[1] - 1)
+    host.set_xticks(range(ys.shape[1]))
+    host.set_xticklabels(ynames, fontsize=14)
+    host.tick_params(axis='x', which='major', pad=7)
+    host.spines['right'].set_visible(False)
+    host.xaxis.tick_top()
+    host.set_title('Parallel Coordinates Plot', fontsize=18)
+
+    colors = plt.cm.tab10.colors
+    # legend_handles = [None for _ in iris.target_names]
+    for j in range(N):
+        if draw_straight:
+            # to just draw straight lines between the axes:
+            host.plot(range(ys.shape[1]), zs[j, :], c=colors[(category[j] - 1) % len(colors)])
+        else:
+            # create bezier curves
+            # for each axis, there will a control vertex at the point itself, one at 1/3rd towards the previous and one
+            #   at one third towards the next axis; the first and last axis have one less control vertex
+            # x-coordinate of the control vertices: at each integer (for the axes) and two inbetween
+            # y-coordinate: repeat every point three times, except the first and last only twice
+            verts = list(zip([x for x in np.linspace(0, len(ys) - 1, len(ys) * 3 - 2, endpoint=True)],
+                             np.repeat(zs[j, :], 3)[1:-1]))
+            # for x,y in verts: host.plot(x, y, 'go') # to show the control points of the beziers
+            codes = [Path.MOVETO] + [Path.CURVE4 for _ in range(len(verts) - 1)]
+            path = Path(verts, codes)
+            patch = patches.PathPatch(path, facecolor='none', lw=1, edgecolor=colors[category[j]])
+            host.add_patch(patch)
+    # host.legend(legend_handles, iris.target_names,
+    #             loc='lower center', bbox_to_anchor=(0.5, -0.18),
+    #             ncol=len(iris.target_names), fancybox=True, shadow=True)
+    plt.tight_layout()
+    plt.show()
+
+
 def extract_incumbents(data, key_group: str = "exp_source", key_performance: str = "cost"):
     incumbents = []
     groups = data.groupby(key_group)
@@ -164,6 +277,7 @@ def extract_incumbents(data, key_group: str = "exp_source", key_performance: str
 
     return incumbents
 
+
 if __name__ == '__main__':
     """
     Assumed folder structure:
@@ -171,7 +285,10 @@ if __name__ == '__main__':
     outdir / smag_logs / run_runid / traj.json
     outdir / trial_setup.json
     """
+    sns.set_style('white')
+
     outdir = "/home/benjamin/Dokumente/code/tmp/carl/src/results/optimized/classic_control/CARLCartPoleEnv/0.1_contexthidden"
+    outdir = "/home/benjamin/Dokumente/code/tmp/CARL/src/results/optimized/classic_control/CARLMountainCarEnv/0.1_contexthidden"
 
     convert_sec_to_hours = True
     key_time = "wallclock_time"
@@ -181,10 +298,13 @@ if __name__ == '__main__':
     data = gather_smac_data(outdir=outdir, key_group=key_group)
     incumbents = extract_incumbents(data=data, key_group=key_group, key_performance=key_performance)
 
-
-
     # plot_smac_trajectory(data=data, key_time=key_time, key_performance=key_performance, key_group=key_group)
-    # plot_parallel_coordinates(data=data, key_hps="incumbent", class_column='seed')
+    plot_parallel_coordinates(data=data, key_hps='incumbent', class_column='seed', draw_straight=True)
+
+
+
+
+
 
 
 
