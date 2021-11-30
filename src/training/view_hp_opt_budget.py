525e6f54d51b87089e52b4cdd24d60b0fb0621a8
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2021-11-30 11:57:01 +0100

    help to select budget/eta

---
+++ src/training/view_hp_opt_budget.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+R = 1000000  # max resource per config
+eta = np.arange(2, 15)
+
+base = eta
+s_max = np.floor(np.log(R) / np.log(base))
+B = (s_max + 1) * R
+
+plt.plot(eta, B, marker='o')
+plt.xlabel("$\eta$")
+plt.ylabel("$B$")
+title = "Select $\eta$ for the maximum budget $B$\nyou would like to spend on the optimization."
+plt.title(title)
+plt.show()
