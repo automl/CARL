e2e687c77f9036e4ade56cdba73f6b3d5d95d021
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2021-12-22 16:18:09 +0100

    current

--- src/play_run.py
+++ src/play_run.py
@@ -2,15 +2,16 @@ from src.train import get_parser, main
 if __name__ == '__main__':
     parser = get_parser()
     args, unknown_args = parser.parse_known_args()
-    args.agent = "SAC"
-    args.env = "CARLBipedalWalkerEnv"
+    args.agent = "PPO"
+    args.env = "CARLMountainCarEnv"
     args.state_context_features = "changing_context_features"
-    args.steps = 200000
-    args.seed = 3
+    args.steps = 300000
+    args.seed = 2
     args.no_eval_callback = True
     args.num_envs = 8
     args.use_xvfb = False
-    args.context_feature_args = ["None"]
+    args.hide_context = False
+    args.context_feature_args = ["g", "m"]
     # args.context_file = "envs/box2d/parking_garage/context_set_all.json"
     print(args)
     main(args, unknown_args, parser)
