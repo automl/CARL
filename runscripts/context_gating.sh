f7eec4597e8f87112c387471bd844c2faaa665fd
Frederik Schubert <frederik.schubert@mailbox.org>
2022-01-08 12:04:16 +0100

    add more content to readme

--- runscripts/context_gating.sh
+++ runscripts/context_gating.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python run_context_gating.py '+experiment=glob(*)' '+algorithm=sac' 'contexts.context_feature_args=[],[g],[max_speed],[l],[m],[dt]' 'carl.state_context_features=null,${contexts.context_feature_args}'  'seed=range(0,5)' 'carl.gaussian_noise_std_percentage=0.4' --multirun
+python run_context_gating.py 'seed=range(0,5)' '+experiment=glob(*)' '+algorithm=sac' '+environment=pendulum' 'contexts.context_feature_args=[],[g],[max_speed],[l],[m],[dt]' 'carl.state_context_features=null,${contexts.context_feature_args}' 'carl.gaussian_noise_std_percentage=0.4' --multirun
