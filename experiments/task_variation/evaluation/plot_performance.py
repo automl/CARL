8a956b8896d5c7515114d771cbc227ab83ac99ff
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-01-21 17:36:04 +0100

    current plotting

--- experiments/task_variation/evaluation/plot_performance.py
+++ experiments/task_variation/evaluation/plot_performance.py
@@ -12,16 +12,17 @@ from experiments.common.eval.gather_data import gather_results
 
 if __name__ == '__main__':
     # path = "/home/benjamin/Dokumente/code/tmp/CARL/src/results/base_vs_context/brax/CARLHalfcheetah"
-    path = "/home/benjamin/Dokumente/code/tmp/CARL/src/results/base_vs_context/classic_control/CARLMountainCarEnv"
+    # path = "/home/benjamin/Dokumente/code/tmp/CARL/src/results/base_vs_context/classic_control/CARLMountainCarEnv"
     # # path = "/home/benjamin/Dokumente/code/tmp/CARL/src/results/base_vs_context/box2d/CARLBipedalWalkerEnv"
     # path = "/home/benjamin/Dokumente/code/tmp/CARL/src/results/base_vs_context/classic_control/CARLPendulumEnv"
-    # path = "/home/benjamin/Dokumente/code/tmp/CARL/src/results/rerun2/base_vs_context/classic_control/CARLPendulumEnv"
-    # path2 = "/home/benjamin/Dokumente/code/tmp/CARL/src/results/compounding/base_vs_context/classic_control/CARLPendulumEnv"
+    path = "/home/benjamin/Dokumente/code/tmp/CARL/src/results/rerun2/base_vs_context/classic_control/CARLPendulumEnv"
+    path2 = "/home/benjamin/Dokumente/code/tmp/CARL/src/results/compounding/base_vs_context/classic_control/CARLPendulumEnv"
     # path = "/home/benjamin/Dokumente/code/tmp/CARL/carl/results/rerun/base_vs_context/brax/CARLHalfcheetah"
     results = gather_results(path=path)
-    # results2 = gather_results(path=path2)
-    # results = pd.concat([results, results2])
+    results2 = gather_results(path=path2)
+    results = pd.concat([results, results2])
 
+    experiment = "compounding"
     paperversion = True
     plot_across_contextfeatures = False
     plot_across_magnitudes = not plot_across_contextfeatures
@@ -48,9 +49,11 @@ if __name__ == '__main__':
 
     figsize = (8, 4) if plot_across_magnitudes else (12, 4)
     if paperversion:
-        figsize = (6, 3)
+        figsize = (4, 3)
         if plot_across_contextfeatures:
-            figsize = (10, 3)
+            figsize = (7, 3)
+        if experiment == "compounding":
+            figsize = (2, 3)
     labelfontsize = 12
     titlefontsize = 12 if not plot_across_contextfeatures else 10
     ticklabelsize = 10
@@ -66,6 +69,19 @@ if __name__ == '__main__':
             return ", ".join([str(el) for el in x])
         results["context_feature_args"] = results["context_feature_args"].apply(cfargs_to_str)
 
+    if experiment == "compounding":
+        forbidden = ["dt", "max_speed", "g", "l"]
+        df_c = results
+        for forb in forbidden:
+            ids = results["context_feature_args"] == forb
+            results = results[~ids]
+
+
+        if plot_across_magnitudes:
+            allowed = 0.5
+            ids = results["context_variation_magnitude"] == allowed
+            df_c = df_c[ids]
+
     env_names = results['env'].unique()
     if len(env_names) > 1:
         raise NotImplementedError("Try to plot across different envs.")
@@ -77,6 +93,8 @@ if __name__ == '__main__':
         fig = plt.figure(figsize=figsize, dpi=200)
         nrows = 1
         ncols = plot_df[key_axgroup].nunique()
+        if experiment == "compounding":
+            ncols += 1
         axes = fig.subplots(nrows=nrows, ncols=ncols, sharey=True)
 
         figtitle = None
@@ -94,7 +112,7 @@ if __name__ == '__main__':
         if "context_feature_args" == key_huegroup:
             legend_title = "varying context feature"
         elif "context_visible" == key_huegroup:
-            legend_title = "context visibility"
+            legend_title = "context"
         title = None
         xlabel = None
         ylabel = None
@@ -140,7 +158,10 @@ if __name__ == '__main__':
                                 subset_group_df = first_none
                 ax = sns.lineplot(data=subset_group_df, x=xname, y=yname, ax=ax, color=color, marker='', hue=hue)
                 legend_handles.append(Line2D([0], [0], color=color))
-                labels.append(df_id)
+                label = df_id
+                if plot_across_contextfeatures:
+                    label = "visible" if label else "hidden"
+                labels.append(label)
 
             # Annotations
             if key_axgroup == "context_variation_magnitude":
@@ -148,7 +169,7 @@ if __name__ == '__main__':
             if key_axgroup == "context_feature_args":
                 title = group_id
             if yname == "episode_reward":
-                ylabel = "mean reward\nacross contexts $\mathcal{C}_{train}$"
+                ylabel = "mean return\nacross contexts $\mathcal{C}_{train}$"
             if title:
                 ax.set_title(title, fontsize=titlefontsize)
             if xlabel:
@@ -169,27 +190,44 @@ if __name__ == '__main__':
                 handle_item = legend_handles.pop(idx)
                 legend_handles.insert(0, handle_item)
 
-            if i == 1:
+            expr = i == 1
+            if experiment == "compounding":
+                expr = True
+            if expr:
                 # labels = [label.replace("__", ", ") for label in labels]
                 ncols = len(legend_handles)
+                title_fontsize = None
+                bbox_to_anchor = (0.5, 0.205)
+                facecolor = None
+                framealpha = None
+                # if experiment == "compounding":
+                #     ncols = 1
+                #     legendfontsize = 8
+                #     title_fontsize = 9
+                #     bbox_to_anchor = (0.4, 0.49)
+                #     framealpha = 0.6
                 legend = fig.legend(
                     handles=legend_handles,
                     labels=labels,
                     loc='lower center',
                     title=legend_title,
-                    ncol=3, #ncols,
+                    ncol=ncols,
                     fontsize=legendfontsize,
                     columnspacing=0.5,
                     handletextpad=0.5,
                     handlelength=1.5,
-                    bbox_to_anchor=(0.5, 0.205)
+                    bbox_to_anchor=bbox_to_anchor,
+                    title_fontsize=title_fontsize,
+                    facecolor=facecolor,
+                    framealpha=framealpha,
                 )
+            print(ax.get_ylim())
 
         if not paperversion:
             fig.suptitle(figtitle)
         fig.set_tight_layout(True)
 
-        if plot_across_contextfeatures:
+        if plot_across_contextfeatures and False:
             index = results["n_context_features"].max()  # plus one to account for default/None
             if index > 1:
                 # Get the bounding boxes of the axes including text decorations
@@ -202,8 +240,16 @@ if __name__ == '__main__':
                 line = plt.Line2D([x, x], [0.05, 0.95], transform=fig.transFigure, color="black")
                 fig.add_artist(line)
 
+        if experiment == "compounding":
+            df_tmp = df_c[df_c[key_plotgroup] == plot_id]
+            ax = sns.lineplot(data=subset_group_df, x=xname, y=yname, ax=ax, color=color, marker='', hue=hue)
 
-        fig_fn = f"evalmeanrew__{env_name}__{key_plotgroup}-{plot_id}.png"
+
+        if experiment is not None:
+            exp_str = str(experiment) + "__"
+        else:
+            exp_str = ""
+        fig_fn = f"{exp_str}evalmeanrew__{env_name}__{key_plotgroup}-{plot_id}.png"
         fig_ffn = Path(path) / fig_fn
         fig.savefig(fig_ffn, bbox_inches="tight")
         print("saved at", fig_ffn)
