c2233d657b3738a0fd3fa315f5f53a380b588470
Theresa Eimer <eimer@tnt.uni-hannover.de>
2022-01-27 11:00:37 +0100

    remove misleading runscripts dir

--- runscripts/context_gating.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python run_context_gating.py 'seed=range(0,5)' '+experiment=glob(*)' '+algorithm=td3' '+environment=ant' 'contexts.context_feature_args=[]' 'carl.state_context_features=null,${contexts.context_feature_args}' 'contexts.default_sample_std_percentage=0.1' --multirun
