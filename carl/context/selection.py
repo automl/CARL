6bcca2dc20b8edc2915506011e126590d9f8b947
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-02-04 08:16:49 +0100

    added docstrings

--- carl/context/selection.py
+++ carl/context/selection.py
@@ -1,40 +1,94 @@
 from abc import abstractmethod
 import numpy as np
 from carl.utils.types import Context
-from typing import Dict, Any
+from typing import Dict, Any, Optional, Tuple, List
 
 
 class AbstractSelector(object):
+    """
+    Base class for context selectors.
+
+    Context is selected when calling `select`, not in `__init__`.
+
+
+    Parameters
+    ----------
+    contexts: Dict[Any, Context]
+        Context set. A `Context` is a Dict[str, Any].
+
+
+    Attributes
+    ----------
+    contexts : Dict[Any, Context]
+        Context set.
+    context_ids : List[int]
+        Integer index for contexts.
+    contexts_keys : List[Any]
+        Keys of contexts dictionary.
+    n_calls : int
+        Number of times `select` has been called.
+    context_id : Optional[int]
+        Context id of current selected context. Is None at first.
+
+    """
     def __init__(self, contexts: Dict[Any, Context]):
-        self.context_ids = np.arange(len(contexts))
-        self.contexts_keys = list(contexts.keys())
-        self.contexts = contexts
-        self.n_calls = 0  # type: int
+        self.contexts: Dict[Any, Context] = contexts
+        self.context_ids: List[int] = list(np.arange(len(contexts)))
+        self.contexts_keys: List[Any] = list(contexts.keys())
+        self.n_calls: int = 0
+        self.context_id: Optional[int] = None  # holds index of current context (integer index of context keys)
 
     @abstractmethod
-    def _select(self) -> Context:
+    def _select(self) -> Tuple[Context, int]:
+        """
+        Select next context (internal).
+
+        Should be implemented in child class, internal use.
+
+        Returns
+        -------
+        context : Context
+            Selected context.
+        context_id : int
+            Integer id of selected context.
+        """
         ...
 
     def select(self) -> Context:
-        context = self._select()
+        """
+        Select next context (API).
+
+        Returns
+        -------
+        context : Context
+            Selected context.
+        """
+        context, context_id = self._select()
+        self.context_id = context_id
         self.n_calls += 1
         return context
 
 
 class RandomSelector(AbstractSelector):
+    """
+    Random Context Selector.
+    """
     def _select(self):
         # TODO seed?
         context_id = np.random.choice(self.context_ids)
         context = self.contexts[self.contexts_keys[context_id]]
-        return context
+        return context, context_id
 
 
 class RoundRobinSelector(AbstractSelector):
-    def __init__(self, contexts: Dict[Any, Context]):
-        super().__init__(contexts=contexts)
-        self.context_id = -1  # type: int
+    """
+    Round robin context selector.
 
+    Iterate through all contexts and then start at the first again.
+    """
     def _select(self):
+        if self.context_id is None:
+            self.context_id = -1
         self.context_id = (self.context_id + 1) % len(self.contexts)
         context = self.contexts[self.contexts_keys[self.context_id]]
-        return context
+        return context, self.context_id
