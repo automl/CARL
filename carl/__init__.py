2a108338bab283b65966620b9c8aa27222c7fcf7
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-02-04 13:27:38 +0100

    author update

--- carl/__init__.py
+++ carl/__init__.py
@@ -2,6 +2,5 @@ __copyright__ = "Copyright 2021, AutoML.org Freiburg-Hannover"
 __license__ = "Apache-2.0 License"
 __version__ = "0.1"
 __author__ = (
-    "Carolin Benjamins and Theresa Eimer, Aditya Mohan, Frederik Schubert, Bodo Rosenhahn, "
-    "André Biedenkapp, Frank Hutter, Marius Lindauer"
+    "Carolin Benjamins, Theresa Eimer, Frederik Schubert, André Biedenkapp, Aditya Mohan"
 )
