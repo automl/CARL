Fix formula of CosineAnnealing
Because sb3 passes remaining progress (1 - current progress), subtract remaining progress from 1 to make it current progress.

experiments/common/train/schedules.py
@@ -7,7 +7,7 @@ class CosineAnnealingLRSchedule(object):
         self.lr_min = lr_min
         self.lr_max = lr_max
 
-    def __call__(self, progress: float) -> float:
+    def __call__(self, remaining_progress: float) -> float:
         """
         Set the learning rate of each parameter group using a cosine annealing
         schedule, where :math:`\eta_{max}` is set to the initial lr and
@@ -28,8 +28,8 @@ class CosineAnnealingLRSchedule(object):
 
         Parameters
         ----------
-        progress : float
-            T_cur / T_max
+        remaining_progress : float
+            1 -> 0. 1 - T_cur / T_max
 
         Returns
         -------
@@ -40,9 +40,22 @@ class CosineAnnealingLRSchedule(object):
         eta_min = self.lr_min
         eta_max = self.lr_max
 
-        eta = eta_min + 0.5 * (eta_max - eta_min) * (1 + np.cos(progress * np.pi))
+        eta = eta_min + 0.5 * (eta_max - eta_min) * (1 + np.cos((1 - remaining_progress) * np.pi))
 
         return eta
+    
+    
+if __name__ == '__main__':
+    T = np.linspace(1, 0, 100)
+    step = np.arange(0, len(T))
+    lr_fn = CosineAnnealingLRSchedule(1e-4, 1e-3)
+    L = lr_fn(T)
+    
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    ax = sns.lineplot(x=step, y=L)
+    ax.set_yscale("log")
+    plt.show()
 
 
 