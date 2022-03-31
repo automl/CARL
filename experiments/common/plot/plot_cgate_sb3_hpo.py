d43e867b51d9d27bc8861e8dd2c003d2d0aa3db0
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-03-31 12:55:55 +0200

    Format black + plot groups

--- experiments/common/plot/plot_cgate_sb3_hpo.py
+++ experiments/common/plot/plot_cgate_sb3_hpo.py
@@ -1,23 +1,32 @@
 from experiments.common.plot.wandb_utils import load_wandb
 from datetime import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Data for cgate sb3 HPO
     # Pendulum 0.1 dt
     project_name = "tnt/carl"
     df_fname = "data/cgate_sb3_hpo.csv"
     config_entries = [
-        "seed"
+        "seed",
         "agent.kwargs.learning_rate",
-        "agent.kwargs.policy_kwargs.context_branch_width"
+        "agent.kwargs.policy_kwargs.context_branch_width",
     ]
     metrics = ["rollout/ep_rew_mean"]
-    timestamp = datetime(2022, 3, 30, hour=17, minute=28, second=0,).timestamp()
+    timestamp = datetime(
+        2022,
+        3,
+        30,
+        hour=17,
+        minute=28,
+        second=0,
+    ).timestamp()
     filters = {
         "state": "finished",
         # "user": "benjamc",
         # "user.username": "benjamc",
-        "summary_metrics._timestamp": {"$gt": timestamp}
+        "summary_metrics._timestamp": {"$gt": timestamp},
     }
     redownload = False
 
@@ -29,3 +38,24 @@ if __name__ == '__main__':
         metrics=metrics,
         config_entries=config_entries,
     )
+
+    groups = data.groupby(
+        [
+            "agent.kwargs.learning_rate",
+            "agent.kwargs.policy_kwargs.context_branch_width",
+        ]
+    )
+
+    sns.set_style("whitegrid")
+    fig = plt.figure(figsize=(6, 4), dpi=200)
+    ax = fig.add_subplot(111)
+    for group_id, group_df in groups:
+        ax = sns.lineplot(
+            data=group_df,
+            x="_step",
+            y="rollout/ep_rew_mean",
+            ax=ax,
+            label=group_id,
+        )
+    fig.set_tight_layout(True)
+    plt.show()
