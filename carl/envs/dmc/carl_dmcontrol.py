05f5058cbf9aa0a2ba521116bc4cef993ab73959
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2022-05-25 17:28:24 +0200

    Add/transform dm_control/Mujoco into CARLEnv
    We could also use the Mujoco envs from gym, but they don't have every env and this way we have more options to modify the envs (it is at least more obvious).
    There are still some open todos.

---
+++ carl/envs/dmc/carl_dmcontrol.py
@@ -0,0 +1,178 @@
+import warnings
+from typing import Any, Dict, List, TypeVar, Union, Optional
+
+import gym
+from gym.envs.classic_control import CartPoleEnv
+
+from carl.envs.carl_env import CARLEnv
+from carl.envs.dmc.wrappers import MujocoToGymWrapper
+from carl.utils.trial_logger import TrialLogger
+from carl.context.selection import AbstractSelector
+
+
+ObsType = TypeVar("ObsType")
+ActType = TypeVar("ActType")
+
+
+"""
+Physics options (defaults for CartPole):
+|           apirate = 100.0                                                │
+│         collision = 0                                                    │
+│              cone = 0                                                    │
+│           density = 0.0                                                  │
+│      disableflags = 0                                                    │
+│       enableflags = 0                                                    │
+│           gravity = array([ 0.  ,  0.  , -9.81])                         │
+│          impratio = 1.0                                                  │
+│        integrator = 0                                                    │
+│        iterations = 100                                                  │
+│          jacobian = 2                                                    │
+│          magnetic = array([ 0. , -0.5,  0. ])                            │
+│    mpr_iterations = 50                                                   │
+│     mpr_tolerance = 1e-06                                                │
+│ noslip_iterations = 0                                                    │
+│  noslip_tolerance = 1e-06                                                │
+│          o_margin = 0.0                                                  │
+│          o_solimp = array([9.0e-01, 9.5e-01, 1.0e-03, 5.0e-01, 2.0e+00]) │
+│          o_solref = array([0.02, 1.  ])                                  │
+│            solver = 2                                                    │
+│          timestep = 0.0025                                               │
+│         tolerance = 1e-08                                                │
+│         viscosity = 0.0                                                  │
+│              wind = array([0., 0., 0.])                                  |
+
+
+C++ Implementation:
+https://mujoco.readthedocs.io/en/latest/APIreference.html#mjoption
+struct _mjOption                    // physics options
+{
+    // timing parameters
+    mjtNum timestep;                // timestep
+    mjtNum apirate;                 // update rate for remote API (Hz)
+
+    // solver parameters
+    mjtNum impratio;                // ratio of friction-to-normal contact impedance
+    mjtNum tolerance;               // main solver tolerance
+    mjtNum noslip_tolerance;        // noslip solver tolerance
+    mjtNum mpr_tolerance;           // MPR solver tolerance
+
+    // physical constants
+    mjtNum gravity[3];              // gravitational acceleration
+    mjtNum wind[3];                 // wind (for lift, drag and viscosity)
+    mjtNum magnetic[3];             // global magnetic flux
+    mjtNum density;                 // density of medium
+    mjtNum viscosity;               // viscosity of medium
+
+    // override contact solver parameters (if enabled)
+    mjtNum o_margin;                // margin
+    mjtNum o_solref[mjNREF];        // solref
+    mjtNum o_solimp[mjNIMP];        // solimp
+
+    // discrete settings
+    int integrator;                 // integration mode (mjtIntegrator)
+    int collision;                  // collision mode (mjtCollision)
+    int cone;                       // type of friction cone (mjtCone)
+    int jacobian;                   // type of Jacobian (mjtJacobian)
+    int solver;                     // solver algorithm (mjtSolver)
+    int iterations;                 // maximum number of main solver iterations
+    int noslip_iterations;          // maximum number of noslip solver iterations
+    int mpr_iterations;             // maximum number of MPR solver iterations
+    int disableflags;               // bit flags for disabling standard features
+    int enableflags;                // bit flags for enabling optional features
+};
+typedef struct _mjOption mjOption;
+"""
+TIMING_PARAMETERS = [
+    "timestep",  # timestep
+    "apirate",  # update rate for remote API (Hz)
+]
+SOLVER_PARAMETERS = [
+    "impratio",  # ratio of friction-to-normal contact impedance
+    "tolerance",  # main solver tolerance
+    "noslip_tolerance",  # noslip solver tolerance
+    "mpr_tolerance",  # MPR solver tolerance
+]
+PHYSICAL_CONSTANTS = [
+    "gravity",
+    "wind",
+    "magnetic",
+    "density",
+    "viscosity",
+]
+OVERRIDE_CONTACT_SOLVER_PARAMETERS = [  # (if enabled)
+    "o_margin",  # margin
+    "o_solref",  # solref
+    "o_solimp",  # solimp
+]
+DISCRETE_SETTINGS = [
+    "integrator",  # integration mode (mjtIntegrator)
+    "collision",  # collision mode (mjtCollision)
+    "cone",  # type of friction cone (mjtCone)
+    "jacobian",  # type of Jacobian (mjtJacobian)
+    "solver",  # solver algorithm (mjtSolver)
+    "iterations",  # maximum number of main solver iterations
+    "noslip_iterations",  # maximum number of noslip solver iterations
+    "mpr_iterations",  # maximum number of MPR solver iterations
+    "disableflags",  # bit flags for disabling standard features
+    "enableflags",  # bit flags for enabling optional features
+]
+
+WORLD_PARAMETERS = (
+    TIMING_PARAMETERS
+    + SOLVER_PARAMETERS
+    + PHYSICAL_CONSTANTS
+    + OVERRIDE_CONTACT_SOLVER_PARAMETERS
+    + TIMING_PARAMETERS
+)
+
+
+DEFAULT_CONTEXT = {}
+
+
+class CARLDmc(CARLEnv):
+    def __init__(
+        self,
+        env: gym.Env = CartPoleEnv(),
+        contexts: Dict[Any, Dict[Any, Any]] = {},
+        hide_context: bool = True,
+        add_gaussian_noise_to_context: bool = False,
+        gaussian_noise_std_percentage: float = 0.01,
+        logger: Optional[TrialLogger] = None,
+        scale_context_features: str = "no",
+        default_context: Optional[Dict] = DEFAULT_CONTEXT,
+        max_episode_length: int = 500,  # from https://github.com/openai/gym/blob/master/gym/envs/__init__.py
+        state_context_features: Optional[List[str]] = None,
+        dict_observation_space: bool = True,
+        context_selector: Optional[Union[AbstractSelector, type(AbstractSelector)]] = None,
+        context_selector_kwargs: Optional[Dict] = None,
+    ):
+        # TODO can we have more than 1 env?
+        env = MujocoToGymWrapper(env)
+        if not contexts:
+            contexts = {0: DEFAULT_CONTEXT}
+        super().__init__(
+            env=env,
+            contexts=contexts,
+            hide_context=hide_context,
+            add_gaussian_noise_to_context=add_gaussian_noise_to_context,
+            gaussian_noise_std_percentage=gaussian_noise_std_percentage,
+            logger=logger,
+            scale_context_features=scale_context_features,
+            default_context=default_context,
+            max_episode_length=max_episode_length,
+            state_context_features=state_context_features,
+            dict_observation_space=dict_observation_space,
+            context_selector=context_selector,
+            context_selector_kwargs=context_selector_kwargs,
+        )
+        self.whitelist_gaussian_noise = list(
+            DEFAULT_CONTEXT.keys()
+        )  # allow to augment all values
+
+    def _update_context(self) -> None:
+        # TODO change parameters of moving model (actuator force etc)
+        for context_name, context_value in self.context:
+            if context_name in WORLD_PARAMETERS:
+                setattr(self.env.physics.model.opt, context_name, context_value)
+            else:
+                warnings.warn(f"Unknown context feature {context_name}.")
---
+++ carl/envs/dmc/wrappers.py
@@ -0,0 +1,110 @@
+from typing import Tuple, Optional, Union
+
+import dm_control.rl
+import gym
+import numpy as np
+from dm_env import StepType
+from gym import spaces
+
+from carl.envs.dmc.carl_dmcontrol import ActType, ObsType
+
+
+class MujocoToGymWrapper(gym.Env):
+    def __init__(self, env: dm_control.rl.control.Environment):
+        # TODO set seeds
+        self.env = env
+
+        action_spec = self.env.action_spec()
+        self.action_space = spaces.Box(action_spec.minimum, action_spec.maximum, dtype=action_spec.dtype)
+
+        obs_spec = self.env.observation_spec()
+        # obs_spaces = {
+        #     k: spaces.Box(low=-np.inf, high=np.inf, shape=v.shape, dtype=v.dtype)
+        #     for k, v in obs_spec.items()
+        # }
+        # self.observation_space = spaces.Dict(spaces=obs_spaces)
+        # TODO add support for Dict Spaces in CARLEnv (later)
+        shapes = [int(np.sum([v.shape for v in obs_spec.values()]))]
+        lows = np.array([-np.inf] * shapes[0])
+        highs = np.array([np.inf] * shapes[0])
+        dtype = np.unique([[v.dtype for v in obs_spec.values()]])[0]
+        self.observation_space = spaces.Box(low=lows, high=highs, shape=shapes, dtype=dtype)
+
+    def step(self, action: ActType) -> Tuple[ObsType, float, bool, dict]:
+        """Run one timestep of the environment's dynamics. When end of
+        episode is reached, you are responsible for calling `reset()`
+        to reset this environment's state.
+
+        Accepts an action and returns a tuple (observation, reward, done, info).
+
+        Args:
+            action (object): an action provided by the agent
+
+        Returns:
+            observation (object): agent's observation of the current environment
+            reward (float) : amount of reward returned after previous action
+            done (bool): whether the episode has ended, in which case further step() calls will return undefined results
+            info (dict): contains auxiliary diagnostic information (helpful for debugging, logging, and sometimes learning)
+        """
+        timestep = self.env.step(action=action)
+        step_type: StepType = timestep.step_type
+        reward = timestep.reward
+        discount = timestep.discount
+        observation = timestep.observation
+        info = {
+            "step_type": step_type,
+            "discount": discount
+        }
+        done = step_type == StepType.LAST
+        return observation, reward, done, info
+
+    def reset(
+        self,
+        *,
+        seed: Optional[int] = None,
+        return_info: bool = False,
+        options: Optional[dict] = None,
+    ) -> Union[ObsType, tuple[ObsType, dict]]:
+        super(MujocoToGymWrapper, self).reset(seed=seed, return_info=return_info, options=options)
+        timestep = self.env.reset()
+        return timestep.observation
+
+    def render(self, mode="human"):
+        """Renders the environment.
+
+        The set of supported modes varies per environment. (And some
+        third-party environments may not support rendering at all.)
+        By convention, if mode is:
+
+        - human: render to the current display or terminal and
+          return nothing. Usually for human consumption.
+        - rgb_array: Return an numpy.ndarray with shape (x, y, 3),
+          representing RGB values for an x-by-y pixel image, suitable
+          for turning into a video.
+        - ansi: Return a string (str) or StringIO.StringIO containing a
+          terminal-style text representation. The text can include newlines
+          and ANSI escape sequences (e.g. for colors).
+
+        Note:
+            Make sure that your class's metadata 'render_modes' key includes
+              the list of supported modes. It's recommended to call super()
+              in implementations to use the functionality of this method.
+
+        Args:
+            mode (str): the mode to render with
+
+        Example:
+
+        class MyEnv(Env):
+            metadata = {'render_modes': ['human', 'rgb_array']}
+
+            def render(self, mode='human'):
+                if mode == 'rgb_array':
+                    return np.array(...) # return RGB frame suitable for video
+                elif mode == 'human':
+                    ... # pop up a window and render
+                else:
+                    super(MyEnv, self).render(mode=mode) # just raise an exception
+        """
+        # TODO render mujoco
+        pass
