dcbdeccb62727b2034cd9bfabd5bbd1028debb72
Sebastian Döhler <sebastiandoehler@hotmail.de>
2022-06-08 15:49:09 +0200

    dmc initializations

---
+++ carl/envs/dmc/__init__.py
@@ -0,0 +1,5 @@
+# Contexts and bounds by name
+from carl.envs.dmc.carl_dm_cartpole import (
+    DEFAULT_CONTEXT as CARLDmcCartPoleEnv_defaults,
+)
+from carl.envs.dmc.carl_dm_cartpole import CARLDmcCartPoleEnv
