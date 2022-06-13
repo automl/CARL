3ff0cec0c3748022f7f48cd8e76444a0c46bb89b
Sebastian Döhler <sebastiandoehler@hotmail.de>
2022-06-13 16:56:06 +0200

    add dmc fish

--- carl/envs/dmc/__init__.py
+++ carl/envs/dmc/__init__.py
@@ -13,3 +13,8 @@ from carl.envs.dmc.carl_dm_quadruped import (
     DEFAULT_CONTEXT as CARLDmcQuadrupedEnv_defaults,
 )
 from carl.envs.dmc.carl_dm_quadruped import CARLDmcQuadrupedEnv
+
+from carl.envs.dmc.carl_dm_fish import (
+    DEFAULT_CONTEXT as CARLDmcFishEnv_defaults,
+)
+from carl.envs.dmc.carl_dm_fish import CARLDmcFishEnv
--- carl/envs/dmc/carl_dm_fish.py
+++ carl/envs/dmc/carl_dm_fish.py
@@ -11,17 +11,16 @@ from carl.envs.dmc.carl_dmcontrol import CARLDmcEnv
 
 DEFAULT_CONTEXT = {
     # TODO update contexts
-    "joint_stiffness": 5000,
-    "gravity_x": 0.,
-    "gravity_y": 0.,
-    "gravity_z": -9.81,
+    # "gravity_x": 0., # gravity disabled in this env
+    # "gravity_y": 0.,
+    # "gravity_z": -9.81,
     "friction_tangential": 0.7,
     "friction_torsional": 0.1,
     "friction_rolling": 0.1,
     "actuator_strength": 1, # scale all actuators by this factor
-    "joint_damping": 0.1,
+    "joint_damping": 0.00002,
     # "torso_mass": 10, # TODO find out if mass can be modified
-    "timestep": 0.005,  # Seconds between updates
+    "timestep": 0.004,  # Seconds between updates
     "magnetic_x": 0., # TODO decide if this is useful
     "magnetic_y": -0.5, 
     "magnetic_z": 0.,
@@ -31,10 +30,9 @@ DEFAULT_CONTEXT = {
 }
 
 CONTEXT_BOUNDS = {
-    "joint_stiffness": (1, np.inf, float),
-    "gravity_x": (-0.1, -np.inf, float),
-    "gravity_y": (-0.1, -np.inf, float),
-    "gravity_z": (-0.1, -np.inf, float),
+    # "gravity_x": (-0.1, -np.inf, float),
+    # "gravity_y": (-0.1, -np.inf, float),
+    # "gravity_z": (-0.1, -np.inf, float),
     "friction_tangential": (-np.inf, np.inf, float), # TODO can friction be negative here?
     "friction_torsional": (-np.inf, np.inf, float),
     "friction_rolling": (-np.inf, np.inf, float),
@@ -50,11 +48,11 @@ CONTEXT_BOUNDS = {
     "wind_z": (-np.inf, np.inf, float),
 }
 
-class CARLDmcQuadrupedEnv(CARLDmcEnv):
+class CARLDmcFishEnv(CARLDmcEnv):
     def __init__(
         self,
-        domain: str = "quadruped",
-        task: str = "walk_context",
+        domain: str = "fish",
+        task: str = "upright_context",
         contexts: Dict[Any, Dict[Any, Any]] = {},
         hide_context: bool = False,
         add_gaussian_noise_to_context: bool = False,
