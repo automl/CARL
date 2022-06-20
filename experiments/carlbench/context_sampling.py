c5da389c2b5d96ada5dc655509f2d1352be20b86
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-06-20 17:45:11 +0200

    Reformat with black

--- experiments/carlbench/context_sampling.py
+++ experiments/carlbench/context_sampling.py
@@ -4,7 +4,6 @@ from pathlib import Path
 import wandb
 from typing import Dict, Any
 import warnings
-import numpy as np
 
 from carl.context.sampling import get_default_context_and_bounds, sample_contexts
 from experiments.common.utils.json_utils import lazy_json_dump
@@ -44,13 +43,13 @@ class AbstractContextSampler(object):
 
 class ContextSampler(AbstractContextSampler):
     def __init__(
-            self,
-            env_name: str,
-            context_feature_names: Optional[Union[List[str], str]] = None,
-            seed: int = 842,
-            difficulty: str = "easy",
-            n_samples: Union[str, int] = 100,
-            sigma_rel: Optional[float] = None,  # overrides difficulty
+        self,
+        env_name: str,
+        context_feature_names: Optional[Union[List[str], str]] = None,
+        seed: int = 842,
+        difficulty: str = "easy",
+        n_samples: Union[str, int] = 100,
+        sigma_rel: Optional[float] = None,  # overrides difficulty
     ):
         """
         Sample contexts for training or evaluation.
@@ -78,7 +77,9 @@ class ContextSampler(AbstractContextSampler):
         self.seed = seed
         self.contexts = None
         self.env_name = env_name
-        self.C_def, self.C_bounds = get_default_context_and_bounds(env_name=self.env_name)
+        self.C_def, self.C_bounds = get_default_context_and_bounds(
+            env_name=self.env_name
+        )
         if context_feature_names is None:
             context_feature_names = []
         elif type(context_feature_names) == str and context_feature_names == "all":
@@ -87,22 +88,27 @@ class ContextSampler(AbstractContextSampler):
 
         if sigma_rel is None:
             if difficulty not in ContextDifficulty.__members__:
-                raise ValueError("Please specify a valid difficulty. Valid options are: ",
-                      list(ContextDifficulty.__members__.keys()))
+                raise ValueError(
+                    "Please specify a valid difficulty. Valid options are: ",
+                    list(ContextDifficulty.__members__.keys()),
+                )
             self.sigma_rel = ContextDifficulty[difficulty].value
         else:
             self.sigma_rel = sigma_rel
 
         if type(n_samples) == str:
             if n_samples not in NContexts.__members__:
-                raise ValueError("Please specify a valid size. Valid options are: ",
-                      list(NContexts.__members__.keys()))
+                raise ValueError(
+                    "Please specify a valid size. Valid options are: ",
+                    list(NContexts.__members__.keys()),
+                )
             self.n_samples = NContexts[n_samples]
         elif type(n_samples) == int or type(n_samples) == float:
             self.n_samples = int(n_samples)
         else:
-            raise ValueError(f"`n_samples` must be of type str "
-                             f"or int, got {type(n_samples)}.")
+            raise ValueError(
+                f"`n_samples` must be of type str " f"or int, got {type(n_samples)}."
+            )
 
         self.contexts = None
 
@@ -117,7 +123,7 @@ class ContextSampler(AbstractContextSampler):
             num_contexts=self.n_samples,
             default_sample_std_percentage=self.sigma_rel,
             context_feature_args=context_feature_args,
-            seed=self.seed
+            seed=self.seed,
         )
         return self.contexts
 
@@ -129,14 +135,13 @@ def log_contexts_json(contexts, path: Union[str, Path]):
 
 
 if __name__ == "__main__":
-    from rich import print
     env_name = "CARLPendulumEnv"
     cs = ContextSampler(
         env_name=env_name,
         difficulty="easy",
         n_samples=1,
         context_feature_names=["m", "l", "g"],
-        seed=455
+        seed=455,
     )
     contexts = cs.sample_contexts()
     print(contexts)
