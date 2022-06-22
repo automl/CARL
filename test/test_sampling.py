00d9840d01d4d317085bf684deff24e7d38da6c2
C. Benjamins <75323339+benjamc@users.noreply.github.com>
2022-06-22 15:21:52 +0200

    Benchmarks (#49)
    * Search in envs module for default context
    
    And bounds. Less hacky now.
    
    * Add test for getting context defaults
    
    * Draft for contexst sampler
    
    * Move function
    
    * Seed sampling of context values
    
    * Pass random generator instead of seed
    
    If seed is passed, we will draw the same values for different context features if they have the same distributions.
    
    * Add basic test for ContextSampler
    
    * Finalize context sampler
    
    * Add README.md
    
    * Add carlbench folder
    
    * Add docstring
    
    * Reformat with black
    
    * Move functions
    
    * Add example yaml config for context sampler
    
    Co-authored-by: Carolin Benjamins <benjamins@tnt.uni-hannover.de>

---
+++ test/test_sampling.py
@@ -0,0 +1,37 @@
+import unittest
+from carl.context.sampling import get_default_context_and_bounds
+from experiments.carlbench.context_sampling import ContextSampler
+
+
+class TestSampling(unittest.TestCase):
+    def test_get_default_context_and_bounds(self):
+        env_name = "CARLPendulumEnv"
+        env_defaults, env_bounds = get_default_context_and_bounds(env_name=env_name)
+        defaults = {'max_speed': 8.0, 'dt': 0.05, 'g': 10.0, 'm': 1.0, 'l': 1.0}
+        self.assertDictEqual(env_defaults, defaults)
+
+    def test_context_sampler(self):
+        env_name = "CARLPendulumEnv"
+        cs = ContextSampler(
+            env_name=env_name,
+            difficulty="easy",
+            n_samples=1,
+            context_feature_names=["m", "l", "g"],
+            seed=455
+        )
+        contexts = cs.sample_contexts()
+        true_dict = {
+                0: {
+                    'max_speed': 8.0,
+                    'dt': 0.05,
+                    'g': 9.748400206554313,
+                    'm': 0.8727822986909317,
+                    'l': 0.9215523401261485
+                }
+            }
+        for context_sampled, context_true in zip(contexts.values(), true_dict.values()):
+            for k in context_sampled.keys():
+                self.assertAlmostEqual(context_sampled[k], context_true[k])
+
+
+
