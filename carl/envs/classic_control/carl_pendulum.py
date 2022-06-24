1bb935ff6e15ce6a6039c943253dbbea60142f75
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-06-24 18:03:05 +0200

    Set hide context to true as default

--- carl/envs/classic_control/carl_pendulum.py
+++ carl/envs/classic_control/carl_pendulum.py
@@ -30,7 +30,7 @@ class CARLPendulumEnv(CARLEnv):
         self,
         env: gym.Env = gccenvs.pendulum.PendulumEnv(),
         contexts: Dict[Any, Dict[Any, Any]] = {},
-        hide_context: bool = False,
+        hide_context: bool = True,
         add_gaussian_noise_to_context: bool = False,
         gaussian_noise_std_percentage: float = 0.01,
         logger: Optional[TrialLogger] = None,
