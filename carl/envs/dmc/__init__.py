11a9050d73fe5be5cf8418ed89d65cb0c13c54d6
sebidoe <45859263+sebidoe@users.noreply.github.com>
2022-06-24 10:54:19 +0200

    Integrate dmcontrol (#47)
    * Add dm_control to requirements
    
    * Add/transform dm_control/Mujoco into CARLEnv
    
    We could also use the Mujoco envs from gym, but they don't have every env and this way we have more options to modify the envs (it is at least more obvious).
    There are still some open todos.
    
    * adapt observations and render rgb
    
    * change dmc load, simplify wrapper
    
    * dmc initializations
    
    * refactor, load context into dmc cartpole
    
    * remove print
    
    * update cartpole context
    
    * general dmc env
    
    * add walker
    
    * fix load dmc
    
    * update walker context
    
    * camera id in render
    
    * dmc test function
    
    * add more context tasks to walker
    
    * add quadruped
    
    * add dmc fish
    
    * walker context defaults, adapt context utils
    
    * update adapt context function
    
    * quadruped adapt context utils
    
    * update dmc context parameters
    
    * rename utils->loader
    
    * add masking to dmc envs
    
    * remove cartpole env
    
    * formatting
    
    * dmc env less duplicate code
    
    * Rename/move file
    
    * Fix tests
    
    Co-authored-by: Carolin Benjamins <benjamins@tnt.uni-hannover.de>

---
+++ carl/envs/dmc/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa: F401
+# Contexts and bounds by name
+from carl.envs.dmc.carl_dm_walker import (
+    CARLDmcWalkerEnv,
+    DEFAULT_CONTEXT as CARLDmcWalkerEnv_defaults,
+    CONTEXT_MASK as CARLDmcWalkerEnv_mask,
+)
+
+from carl.envs.dmc.carl_dm_quadruped import (
+    CARLDmcQuadrupedEnv,
+    DEFAULT_CONTEXT as CARLDmcQuadrupedEnv_defaults,
+    CONTEXT_MASK as CARLDmcQuadrupedEnv_mask,
+)
+
+from carl.envs.dmc.carl_dm_fish import (
+    CARLDmcFishEnv,
+    DEFAULT_CONTEXT as CARLDmcFishEnv_defaults,
+    CONTEXT_MASK as CARLDmcFishEnv_mask,
+)
---
+++ carl/envs/dmc/carl_dm_fish.py
@@ -0,0 +1,88 @@
+from typing import Any, Dict, List, Optional, Union
+
+import numpy as np
+
+from carl.utils.trial_logger import TrialLogger
+from carl.context.selection import AbstractSelector
+from carl.envs.dmc.carl_dmcontrol import CARLDmcEnv
+
+
+DEFAULT_CONTEXT = {
+    "gravity": -9.81,  # Gravity is disabled via flag
+    "friction_tangential": 1,  # Scaling factor for tangential friction of all geoms (objects)
+    "friction_torsional": 1,  # Scaling factor for torsional friction of all geoms (objects)
+    "friction_rolling": 1,  # Scaling factor for rolling friction of all geoms (objects)
+    "timestep": 0.004,  # Seconds between updates
+    "joint_damping": 1.,  # Scaling factor for all joints
+    "joint_stiffness": 0.,
+    "actuator_strength": 1,  # Scaling factor for all actuators in the model
+    "density": 5000.,
+    "viscosity": 0.,
+    "geom_density": 1.,  # No effect, because no gravity
+    "wind_x": 0.,
+    "wind_y": 0.,
+    "wind_z": 0.,
+}
+
+CONTEXT_BOUNDS = {
+    "gravity": (-0.1, -np.inf, float),
+    "friction_tangential": (0, np.inf, float),
+    "friction_torsional": (0, np.inf, float),
+    "friction_rolling": (0, np.inf, float),
+    "timestep": (0.001, 0.1, float,),
+    "joint_damping": (0, np.inf, float),
+    "joint_stiffness": (0, np.inf, float),
+    "actuator_strength": (0, np.inf, float),
+    "density": (0, np.inf, float),
+    "viscosity": (0, np.inf, float),
+    "geom_density": (0, np.inf, float),
+    "wind_x": (-np.inf, np.inf, float),
+    "wind_y": (-np.inf, np.inf, float),
+    "wind_z": (-np.inf, np.inf, float),
+}
+
+CONTEXT_MASK = [
+    "gravity",
+    "geom_density",
+    "wind_x",
+    "wind_y",
+    "wind_z",
+]
+
+
+class CARLDmcFishEnv(CARLDmcEnv):
+    def __init__(
+        self,
+        domain: str = "fish",
+        task: str = "upright_context",
+        contexts: Dict[Any, Dict[Any, Any]] = {},
+        context_mask: Optional[List[str]] = [],
+        hide_context: bool = False,
+        add_gaussian_noise_to_context: bool = False,
+        gaussian_noise_std_percentage: float = 0.01,
+        logger: Optional[TrialLogger] = None,
+        scale_context_features: str = "no",
+        default_context: Optional[Dict] = DEFAULT_CONTEXT,
+        max_episode_length: int = 500,  # from https://github.com/openai/gym/blob/master/gym/envs/__init__.py
+        state_context_features: Optional[List[str]] = None,
+        dict_observation_space: bool = False,
+        context_selector: Optional[Union[AbstractSelector, type(AbstractSelector)]] = None,
+        context_selector_kwargs: Optional[Dict] = None,
+    ):
+        super().__init__(
+            domain=domain,
+            task=task,
+            contexts=contexts,
+            context_mask=context_mask,
+            hide_context=hide_context,
+            add_gaussian_noise_to_context=add_gaussian_noise_to_context,
+            gaussian_noise_std_percentage=gaussian_noise_std_percentage,
+            logger=logger,
+            scale_context_features=scale_context_features,
+            default_context=default_context,
+            max_episode_length=max_episode_length,
+            state_context_features=state_context_features,
+            dict_observation_space=dict_observation_space,
+            context_selector=context_selector,
+            context_selector_kwargs=context_selector_kwargs,
+        )
