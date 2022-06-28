6914582d0a33c717606dca2f87475f98ce5f977f
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2022-06-28 15:06:51 +0200

    Load wandb table to df and context dict

--- experiments/carlbench/context_logging.py
+++ experiments/carlbench/context_logging.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 from typing import Dict, Any, Union
 import wandb
+import numpy as np
+import pandas as pd
 
-from experiments.common.utils.json_utils import lazy_json_dump
+from experiments.common.utils.json_utils import lazy_json_dump, lazy_json_load
 
 
 def log_contexts_wandb_traineval(train_contexts, eval_contexts):
@@ -25,3 +27,22 @@ def log_contexts_json(contexts, path: Union[str, Path]):
     path = Path(path)
     path.parent.mkdir(exist_ok=True, parents=True)
     lazy_json_dump(contexts, str(path))
+
+
+def load_wandb_table_to_df(path: Union[str, Path]) -> pd.DataFrame:
+    data = lazy_json_load(path)
+    df = pd.DataFrame(data=np.array(data["data"]), columns=data["columns"])
+    return df
+
+
+def load_wandb_contexts(path: Union[str, Path]) -> Dict[Any, Dict[Any, Any]]:
+    data = lazy_json_load(path)
+    cols = data["columns"]
+    D = data["data"]
+    contexts = {
+        i: {
+            cols[j]: v for j, v in enumerate(context_values)
+        } for i, context_values in enumerate(D)
+    }
+    return contexts
+
