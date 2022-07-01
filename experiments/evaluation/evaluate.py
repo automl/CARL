058bf1bfb09e079044099d5a5a7b0f655666d672
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-07-01 15:15:03 +0200

    Include wandb again

--- experiments/evaluation/evaluate.py
+++ experiments/evaluation/evaluate.py
@@ -61,29 +61,29 @@ def train(cfg: DictConfig):
 
     wandbdir = Path(cfg.results_path) / "wandb"
 
-    # run = wandb.init(
-    #     id=cfg.wandb.id,
-    #     resume="allow",
-    #     mode="offline" if cfg.wandb.debug else None,
-    #     project=cfg.wandb.project,
-    #     job_type=cfg.wandb.job_type,
-    #     entity=cfg.wandb.entity,
-    #     group=cfg.wandb.group,  # TODO get group from traincfg
-    #     dir=os.getcwd(),
-    #     config=OmegaConf.to_container(cfg, resolve=True, enum_to_str=True),
-    #     sync_tensorboard=True,
-    #     monitor_gym=True,
-    #     save_code=True,
-    #     tags=cfg.wandb.tags,
-    #     notes=cfg.wandb.notes,
-    # )
+    run = wandb.init(
+        id=cfg.wandb.id,
+        resume="allow",
+        mode="offline" if cfg.wandb.debug else None,
+        project=cfg.wandb.project,
+        job_type=cfg.wandb.job_type,
+        entity=cfg.wandb.entity,
+        group=traincfg.wandb.group, 
+        dir=os.getcwd(),
+        config=OmegaConf.to_container(cfg, resolve=True, enum_to_str=True),
+        sync_tensorboard=True,
+        monitor_gym=True,
+        save_code=True,
+        tags=cfg.wandb.tags,
+        notes=cfg.wandb.notes,
+    )
     hydra_cfg = HydraConfig.get()
     command = f"{hydra_cfg.job.name}.py " + " ".join(hydra_cfg.overrides.task)
     if not OmegaConf.is_missing(hydra_cfg.job, "id"):
         slurm_id = hydra_cfg.job.id
     else:
         slurm_id = None
-    # wandb.config.update({"command": command, "slurm_id": slurm_id})
+    wandb.config.update({"command": command, "slurm_id": slurm_id})
     set_seed_everywhere(traincfg.seed)
 
     # ----------------------------------------------------------------------
@@ -120,7 +120,7 @@ def train(cfg: DictConfig):
     else:
         contexts = lazy_json_load(cfg.contexts_path)
     if contexts is not None:
-        # log_contexts_wandb(contexts=contexts, wandb_key="evalpost/contexts")
+        log_contexts_wandb(contexts=contexts, wandb_key="evalpost/contexts")
         cfg.n_eval_episodes = cfg.n_eval_episodes_per_context * len(contexts)
 
     # ----------------------------------------------------------------------
@@ -165,7 +165,7 @@ def train(cfg: DictConfig):
     # ----------------------------------------------------------------------
     # Cleanup
     # ----------------------------------------------------------------------
-    # run.finish()
+    run.finish()
 
     return avg_return
 
