989731472082bca764bd10050bac6e3bbb460723
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2022-07-06 14:03:53 +0200

    Update carracing example

--- examples/demo_carracing.py
+++ examples/demo_carracing.py
@@ -1,45 +1,47 @@
+"""
+Code adapted from gym.envs.box2d.car_racing.py
+"""
+
 from typing import Any
 import numpy as np
 import gym
 import time
+import pygame
 from carl.envs.box2d.carl_vehicle_racing import CARLVehicleRacingEnv, VEHICLE_NAMES
-
 
 if __name__ == "__main__":
     from pyglet.window import key
 
     a = np.array([0.0, 0.0, 0.0])
 
-    def key_press(k: int, mod: Any) -> None:
-        global restart
-        if k == 0xFF0D:
-            restart = True
-        if k == key.SPACE:
-            restart = True
-        if k == key.LEFT:
-            a[0] = -1.0
-        if k == key.RIGHT:
-            a[0] = +1.0
-        if k == key.UP:
-            a[1] = +1.0
-        if k == key.DOWN:
-            a[2] = +1.0
+    def register_input():
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    a[0] = -1.0
+                if event.key == pygame.K_RIGHT:
+                    a[0] = +1.0
+                if event.key == pygame.K_UP:
+                    a[1] = +1.0
+                if event.key == pygame.K_DOWN:
+                    a[2] = +0.8  # set 1.0 for wheels to block to zero rotation
+                if event.key == pygame.K_RETURN:
+                    global restart
+                    restart = True
 
-    def key_release(k: int, mod: Any) -> None:
-        if k == key.LEFT and a[0] == -1.0:
-            a[0] = 0
-        if k == key.RIGHT and a[0] == +1.0:
-            a[0] = 0
-        if k == key.UP:
-            a[1] = 0
-        if k == key.DOWN:
-            a[2] = 0
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    a[0] = 0
+                if event.key == pygame.K_RIGHT:
+                    a[0] = 0
+                if event.key == pygame.K_UP:
+                    a[1] = 0
+                if event.key == pygame.K_DOWN:
+                    a[2] = 0
 
     contexts = {i: {"VEHICLE": i} for i in range(len(VEHICLE_NAMES))}
     env = CARLVehicleRacingEnv(contexts=contexts)
     env.render()
-    env.viewer.window.on_key_press = key_press
-    env.viewer.window.on_key_release = key_release
     record_video = False
     if record_video:
         from gym.wrappers.record_video import RecordVideo
@@ -53,6 +55,7 @@ if __name__ == "__main__":
         steps = 0
         restart = False
         while True:
+            register_input()
             s, r, done, info = env.step(a)
             time.sleep(0.025)
             total_reward += r
