98ed0b33bf10ea62834411fb4b7f20b910c00605
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2022-07-06 12:27:29 +0200

    Fix mypy brax

--- carl/utils/doc_building/render_brax_env.py
+++ carl/utils/doc_building/render_brax_env.py
@@ -1,13 +1,12 @@
 if __name__ == "__main__":
     from typing import List
-
-    import brax
     import jax
+    import brax
     from brax import envs
     from brax.io import html
     from IPython.display import HTML
 
-    env_name = "fetch"  # @param ['ant', 'humanoid', 'fetch', 'grasp', 'halfcheetah', 'ur5e', 'reacher']
+    env_name = "ant"  # @param ['ant', 'humanoid', 'halfcheetah', ...]
     env_fn = envs.create_fn(env_name=env_name)
     env = env_fn()
     state = env.reset(rng=jax.random.PRNGKey(seed=1))
