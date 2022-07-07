fe8ff535bc7d6f58964134b937f2bc6892c71819
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-07-07 07:57:50 +0200

    Find paths in function

--- experiments/evaluation/run_evaluation.py
+++ experiments/evaluation/run_evaluation.py
@@ -9,10 +9,34 @@ from subprocess import Popen, DEVNULL
 from rich import print
 import glob
 import regex as re
+from typing import List
 
 
 def glob_re(pattern, strings):
     return filter(re.compile(pattern).match, strings)
+
+
+def find_multirun_paths(result_dir: str) -> List[str]:
+    """
+    Find all folders with a number as name
+
+    These folders are the folders from the hydra multirun.
+
+    Parameters
+    ----------
+    result_dir : str
+        Path to hydra result dir containing multirun folders.
+
+    Returns
+    -------
+    List[str]
+        Paths to individual runs
+
+    """
+    filenames = list(glob_re(r"\d\d*", os.listdir(result_dir)))
+    filenames.sort(key=float)
+    result_paths = [f"{os.path.join(result_dir, f)}" for f in filenames]
+    return result_paths
 
 
 def main():
@@ -34,9 +58,8 @@ def main():
 
     # Find subfolders with multiruns
     if args.result_dir is not None:
-        filenames = list(glob_re(r"\d\d*", os.listdir(args.result_dir)))
-        filenames.sort(key=float)
-        result_paths = [f"'{os.path.join(args.result_dir, f)}'" for f in filenames]
+        result_paths = find_multirun_paths(result_dir=args.result_dir)
+        result_paths = [f"'{p}'" for p in result_paths]
         result_paths_str = ",".join(result_paths)
         result_paths_arg = f"results_path={result_paths_str}"
         unknown_args.append(result_paths_arg)
