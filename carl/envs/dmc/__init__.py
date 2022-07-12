5bc8f2417114b3a21962109e53df9f0729c0d4e7
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2022-07-12 14:59:35 +0200

    Import context bounds

--- carl/envs/dmc/__init__.py
+++ carl/envs/dmc/__init__.py
@@ -2,12 +2,15 @@
 # Contexts and bounds by name
 from carl.envs.dmc.carl_dm_fish import CONTEXT_MASK as CARLDmcFishEnv_mask
 from carl.envs.dmc.carl_dm_fish import DEFAULT_CONTEXT as CARLDmcFishEnv_defaults
+from carl.envs.dmc.carl_dm_fish import CONTEXT_BOUNDS as CARLDmcFishEnv_bounds
 from carl.envs.dmc.carl_dm_fish import CARLDmcFishEnv
 from carl.envs.dmc.carl_dm_quadruped import CONTEXT_MASK as CARLDmcQuadrupedEnv_mask
 from carl.envs.dmc.carl_dm_quadruped import (
     DEFAULT_CONTEXT as CARLDmcQuadrupedEnv_defaults,
 )
 from carl.envs.dmc.carl_dm_quadruped import CARLDmcQuadrupedEnv
+from carl.envs.dmc.carl_dm_quadruped import CONTEXT_BOUNDS as CARLDmcQuadrupedEnv_bounds
 from carl.envs.dmc.carl_dm_walker import CONTEXT_MASK as CARLDmcWalkerEnv_mask
 from carl.envs.dmc.carl_dm_walker import DEFAULT_CONTEXT as CARLDmcWalkerEnv_defaults
 from carl.envs.dmc.carl_dm_walker import CARLDmcWalkerEnv
+from carl.envs.dmc.carl_dm_walker import CONTEXT_BOUNDS as CARLDmcWalkerEnv_bounds
