6c2c2eb8248e87ecdb654096ce9d37debc59d301
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-07-21 16:16:24 +0200

    More sys paths

--- experiments/attack_on_agents/run_perturbance_optimization.py
+++ experiments/attack_on_agents/run_perturbance_optimization.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 print(sys.path)
 import numpy as np
