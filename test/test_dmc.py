0cd45e3569e7d86247ff0699cedce810f4a4b37c
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2022-09-09 20:52:02 +0200

    Add more tests

--- test/test_dmc.py
+++ test/test_dmc.py
@@ -1,6 +1,7 @@
 import unittest
 
 from carl.envs.dmc.loader import load_dmc_env
+from carl.envs.dmc.dmc_tasks.finger import check_constraints, spin_context, turn_easy_context, turn_hard_context
 
 
 class TestDMCLoader(unittest.TestCase):
@@ -29,3 +30,20 @@ class TestDMCLoader(unittest.TestCase):
                 domain_name="sdfsdf",
                 task_name="walk",
             )
+
+
+class TestDmcEnvs(unittest.TestCase):
+    def test_finger_constraints(self):
+        # Finger can reach spinner?
+        with self.assertRaises(ValueError):
+            check_constraints(limb_length_0=0.17, limb_length_1=0.16, spinner_length=0.1)
+        # Spinner collides with finger hinge?
+        with self.assertRaises(ValueError):
+            check_constraints(limb_length_0=0.17, limb_length_1=0.16, spinner_length=0.81)
+
+    def test_finger_tasks(self):
+        tasks = [spin_context, turn_hard_context, turn_easy_context]
+        contexts = [{}, {"spinner_length": 0.2}]
+        for context in contexts:
+            for task in tasks:
+                _ = task(context=context)
