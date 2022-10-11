6b1520dc91bd106f6630bffa47c800b11228db5d
amsks <adityak735@gmail.com>
2022-10-11 22:03:19 +0200

    added submodule

---
+++ carl/envs/rna/__init__.py
@@ -0,0 +1,9 @@
+
+try:
+    from carl.envs.rna.carl_rna import CARLRnaDesignEnv
+    from carl.envs.rna.carl_rna_definitions import (
+        DEFAULT_CONTEXT as CARLRnaDesignEnv_defaults,
+        CONTEXT_BOUNDS as CARLRnaDesignEnv_bounds,
+    )
+except Exception as e:
+    print(e)
