e21c69b57ff4ab75e2e5c6d418156d64769fa3ad
C. Benjamins <75323339+benjamc@users.noreply.github.com>
2022-11-03 10:15:26 +0100

    Fix format, fix tests (#73)
    * Fix format, fix tests
    
    * Fix format
    
    * Increase coverage
    
    * Remove test code
    
    * Add more tests
    
    Co-authored-by: Carolin Benjamins <benjamins@tnt.uni-hannover.de>

--- test/test_CARLEnv.py
+++ test/test_CARLEnv.py
@@ -26,6 +26,48 @@ class TestStateConstruction(unittest.TestCase):
         env = CARLPendulum(obs_context_features=context_keys)
         state, info = env.reset()
         self.assertEqual(len(state["context"]), n)
+        
+
+    def test_get_context_key(self):
+        contexts = self.generate_contexts()
+        env = CARLPendulumEnv(contexts=contexts)
+        self.assertEqual(env.context_key, None)
+
+
+class TestContextSampler(unittest.TestCase):
+    def test_get_defaults(self):
+        from carl.context.sampling import get_default_context_and_bounds
+
+        defaults, bounds = get_default_context_and_bounds(env_name="CARLPendulumEnv")
+        DEFAULT_CONTEXT = {
+            "max_speed": 8.0,
+            "dt": 0.05,
+            "g": 10.0,
+            "m": 1.0,
+            "l": 1.0,
+            "initial_angle_max": np.pi,
+            "initial_velocity_max": 1,
+        }
+        self.assertDictEqual(defaults, DEFAULT_CONTEXT)
+
+    def test_sample_contexts(self):
+        from carl.context.sampling import sample_contexts
+
+        contexts = sample_contexts(
+            env_name="CARLPendulumEnv",
+            context_feature_args=["l"],
+            num_contexts=1,
+            default_sample_std_percentage=0.0,
+        )
+        self.assertEqual(contexts[0]["l"], 1)
+
+
+class TestContextAugmentation(unittest.TestCase):
+    def test_gaussian_noise(self):
+        from carl.context.augmentation import add_gaussian_noise
+
+        c = add_gaussian_noise(default_value=1, percentage_std=0)
+        self.assertEqual(c, 1)
 
 
 if __name__ == "__main__":
