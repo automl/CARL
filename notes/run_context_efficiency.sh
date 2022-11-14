c8a81f1e94e192dad029395b2713f357da1407a9
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2022-11-14 20:12:09 +0100

    Fix commands

--- notes/run_context_efficiency.sh
+++ notes/run_context_efficiency.sh
@@ -1,12 +1,12 @@
 echo "Start Training"
 echo "-----------------------------------------"
-export env_name=CARLPendulum
+export env_name=$1
 python experiments/benchmarking/run_training.py 'seed=range(1,11)' '+experiments=context_efficiency' 'context_sampler.n_samples=1,2,4,8,16,32,64,128,256,512' '+context_visibility=hidden' --snap_dir ./runs/context_efficiency/${env_name}/train -m
 
 
 echo "Evaluate on Train Contexts"
 echo "-----------------------------------------"
-python experiments/evaluation/run_evaluation.py '+experiments=context_efficiency' folder_id=${env_name}/on_train  --result_dir ./runs/context_efficiency/${env_name}/train
+python experiments/evaluation/run_evaluation.py '+experiments=context_efficiency' folder_id=${env_name}/eval/on_train  --result_dir ./runs/context_efficiency/${env_name}/train -m
 
 
 echo "Create Test Contexts"
@@ -18,4 +18,4 @@ export contexts_path=runs/context_efficiency/${env_name}/contexts_evaluation.jso
 
 echo "Evaluate on Test Contexts"
 echo "-----------------------------------------"
-python experiments/evaluation/run_evaluation.py '+experiments=context_efficiency' contexts_path=${contexts_path} folder_id=${env_name}/on_test  --result_dir ./runs/context_efficiency/${env_name}/train
+python experiments/evaluation/run_evaluation.py '+experiments=context_efficiency' contexts_path=${contexts_path} folder_id=${env_name}/eval/on_test  --result_dir ./runs/context_efficiency/${env_name}/train -m
