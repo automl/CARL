34cfb1a20f79037f6b00a5b6c828d680d302334e
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2022-11-15 16:26:07 +0100

    Fix number of table entries

--- experiments/evaluation/evaluate.py
+++ experiments/evaluation/evaluate.py
@@ -180,7 +180,7 @@ def evaluate_policy(cfg: DictConfig):
         skip_eval = True
 
     wandb.Table.MAX_ROWS = 50000
-    table_entries = len(contexts) * cfg.n_eval_episodes 
+    table_entries = cfg.n_eval_episodes 
     if table_entries > wandb.Table.MAX_ROWS:
         raise ValueError(f"Wandb table too small ({wandb.Table.MAX_ROWS}) for {table_entries}. Increase wandb.Table.MAX_ROWS.")
 
