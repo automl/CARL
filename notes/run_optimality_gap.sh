19cf2f6d0561a4ebe55df536fe08c30632161b58
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2022-11-18 11:36:50 +0100

    Fix commands

--- notes/run_optimality_gap.sh
+++ notes/run_optimality_gap.sh
@@ -6,7 +6,7 @@ python experiments/benchmarking/run_training.py 'seed=range(1,11)' '+experiments
 
 echo "Evaluate General Agent on Train Contexts"
 echo "-----------------------------------------"
-python experiments/evaluation/run_evaluation.py '+experiments=optimality_gap' folder_id=${env_name}/eval_general  --result_dir ./runs/optimality_gap/${env_name}/train_general
+python experiments/evaluation/run_evaluation.py '+experiments=optimality_gap' folder_id=${env_name}/eval_general  --result_dir ./runs/optimality_gap/${env_name}/train_general -m
 
 
 echo "Create Runcommands to Train Oracle"
@@ -14,8 +14,11 @@ echo "-----------------------------------------"
 python experiments/benchmarking/create_optimality_gap_runcommands_oracle.py $env_name
 
 
+echo "Train Oracle"
+echo "-----------------------------------------"
 bash ./runs/optimality_gap/${env_name}/runcommands/run_train_oracles.sh
+
 
 echo "Evaluate Oracles on Train Contexts"
 echo "-----------------------------------------"
-python experiments/evaluation/run_evaluation.py '+experiments=optimality_gap' folder_id=${env_name}/eval_oracle  --result_dir ./runs/optimality_gap/${env_name}/train_oracle
+python experiments/evaluation/run_evaluation.py '+experiments=optimality_gap' folder_id=${env_name}/eval_oracle  --result_dir ./runs/optimality_gap/${env_name}/train_oracle -m
