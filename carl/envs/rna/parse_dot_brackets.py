6a237b6a6c6f33ba7c1a94f82ba9d484b291e253
amsks <adityak735@gmail.com>
2022-11-23 10:59:15 +0100

    Formatted dot brackets generator

--- carl/envs/rna/parse_dot_brackets.py
+++ carl/envs/rna/parse_dot_brackets.py
@@ -1,20 +1,34 @@
 from pathlib import Path
 
+from typing import List, Optional, Union, Generator
+
 
 def parse_dot_brackets(
-    dataset, data_dir, target_structure_ids=None, target_structure_path=None
-):
-    """TODO
-    Generate the targets for next epoch.
-
-    Args:
-        dataset: The name of the benchmark to use targets from.
-        data_dir: The directory of the target structures.
-        target_structure_ids: Use specific targets by ids.
-        target path: Specify a path to the targets.
+    dataset: str,
+    data_dir: str,
+    target_structure_ids: Optional[Union[List[int], int]] = None,
+    target_structure_path: Optional[str] = None,
+) -> Generator[int]:
+    """Generate the targets for next epoch.
 
     Returns:
+
+    Parameters
+    ----------
+    dataset : str
+        The name of the benchmark to use targets from
+    data_dir : str
+        The directory of the target structures.
+    target_structure_ids : Optional[Union[List[int], int]], optional
+        Use specific targets by ids., by default None
+    target_structure_path : Optional[str], optional
+        pecify a path to the targets., by default None
+
+    Returns
+    -------
+    Generator[int]
         An epoch generator for the specified target structures.
+
     """
     if target_structure_path:
         target_paths = [target_structure_path]
@@ -25,4 +39,9 @@ def parse_dot_brackets(
     else:
         target_paths = list(Path(data_dir, dataset).glob("*.rna"))
 
-    return [data_path.read_text().rstrip() for data_path in target_paths]
+    x = [data_path.read_text().rstrip() for data_path in target_paths]
+    import pdb
+
+    pdb.set_trace()
+
+    return x
