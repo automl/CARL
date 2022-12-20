b2bffde93f6bb3016a353c0a2c831976254da149
amsks <adityak735@gmail.com>
2022-12-20 10:02:45 +0100

    RNA design environmentclear

--- carl/envs/rna/rna_environment.py
+++ carl/envs/rna/rna_environment.py
@@ -16,6 +16,8 @@ import gymnasium as gym
 from typing import Any, List
 import pdb
 
+import pdb
+
 
 @dataclass
 class RnaDesignEnvironmentConfig:
@@ -83,7 +85,9 @@ def _encode_dot_bracket(  # type: ignore[no-untyped-def]
     # Sites corresponds to 1 pixel with 1 channel if convs are applied directly
     if env_config.use_conv and not env_config.use_embedding:
         return [[site_encoding[site]] for site in padded_secondary]
-
+    
+    pdb.set_trace()
+    
     return [site_encoding[site] for site in padded_secondary]
 
 
