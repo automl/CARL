2ec09a7cb9110a4457e36ce0c0f2277821a4151b
amsks <adityak735@gmail.com>
2023-02-22 16:21:39 +0100

    pre-commit issues fixed

--- carl/envs/rna/parse_dot_brackets.py
+++ carl/envs/rna/parse_dot_brackets.py
@@ -38,4 +38,4 @@ def parse_dot_brackets(
     else:
         target_paths = list(Path(data_dir, dataset).glob("*.rna"))
 
-    return [data_path.read_text().rstrip() for data_path in target_paths]
+    return [data_path.read_text().rstrip() for data_path in target_paths]  # type: ignore[union-attr]
