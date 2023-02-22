3d79a0e6d5f3ed19e54a063189bc5f8aaa1e82e6
amsks <adityak735@gmail.com>
2023-02-22 16:21:39 +0100

    pre-commit issues fixed

--- carl/envs/rna/carl_rna.py
+++ carl/envs/rna/carl_rna.py
@@ -21,8 +21,6 @@ from carl.context.selection import AbstractSelector
 ACTION_SPACE = gym.spaces.Discrete(4)
 OBSERVATION_SPACE = gym.spaces.Box(low=-np.inf * np.ones(11), high=np.inf * np.ones(11))
 
-
-# TODO: mypy
 class CARLRnaDesignEnv(CARLEnv):
     def __init__(
         self,
