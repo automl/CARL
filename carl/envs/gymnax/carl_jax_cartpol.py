85ac30289d06a125d1a75d03d18322810d27d9d1
Abbas Dehghan <abbasdehghanki@gmail.com>
2023-04-27 16:24:27 +0200

    add gymnax cartpol

--- carl/envs/gymnax/carl_jax_cartpol.py
+++ carl/envs/gymnax/carl_jax_cartpol.py
@@ -1,41 +1,38 @@
-from typing import Dict, List, Optional, Union 
-
+from typing import Dict, List, Optional, Union
 
 import jax.numpy as jnp
 import numpy as np
+from gymnax.environments.classic_control.cartpole import CartPole
 
 from carl.context.selection import AbstractSelector
 from carl.envs.carl_env import CARLEnv
 from carl.utils.trial_logger import TrialLogger
 from carl.utils.types import Context, Contexts
-from gymnax.environments import spaces
-from gymnax.environments.classic_control.pendulum import Pendulum, EnvParams
 
 DEFAULT_CONTEXT = {
-    "max_speed": 8.0,
-    "max_torque": 2.0,
-    "dt": 0.05,
-    "g": 10.0,
-    "m": 1.0,
-    "l": 1.0,
-    "max_steps_in_episode": 200,
+    "gravity": 9.8,
+    "masscart": 1.0,
+    "masspole": 0.1,
+    "length": 0.5,
+    "force_mag": 10.0,
+    "tau": 0.02,
 }
 
 CONTEXT_BOUNDS = {
-    "max_speed": (-np.inf, np.inf, float),
-    "max_torque": (-np.inf, np.inf, float),
-    "dt": (0, np.inf, float),
-    "g": (0, np.inf, float),
-    "m": (1e-6, np.inf, float),
-    "l": (1e-6, np.inf, float),
-    "max_steps_in_episode": (1, np.inf, int),
+    "gravity": (5.0, 15.0, float),
+    "masscart": (0.5, 2.0, float),
+    "masspole": (0.05, 0.2, float),
+    "length": (0.25, 1.0, float),
+    "force_mag": (5.0, 15.0, float),
+    "tau": (0.01, 0.05, float),
 }
 
 
-class CARLJaxPendulumEnv(CARLEnv): 
+
+class CARLCartPoleEnv(CARLEnv):
     def __init__(
         self,
-        env: Pendulum = Pendulum(),
+        env: CartPole = CartPole(),
         contexts: Contexts = {},
         hide_context: bool = True,
         add_gaussian_noise_to_context: bool = False,
@@ -43,7 +40,7 @@ class CARLJaxPendulumEnv(CARLEnv):
         logger: Optional[TrialLogger] = None,
         scale_context_features: str = "no",
         default_context: Optional[Context] = DEFAULT_CONTEXT,
-        max_episode_length: int = 200,  # from https://github.com/openai/gym/blob/master/gym/envs/__init__.py
+        max_episode_length: int = 500,  # from https://github.com/openai/gym/blob/master/gym/envs/__init__.py
         state_context_features: Optional[List[str]] = None,
         context_mask: Optional[List[str]] = None,
         dict_observation_space: bool = False,
@@ -52,18 +49,6 @@ class CARLJaxPendulumEnv(CARLEnv):
         ] = None,
         context_selector_kwargs: Optional[Dict] = None,
     ):
-        """
-        Max torque is not a context feature because it changes the action space.
-
-        Parameters
-        ----------
-        env
-        contexts
-        instance_mode
-        hide_context
-        add_gaussian_noise_to_context
-        gaussian_noise_std_percentage
-        """
         if not contexts:
             contexts = {0: DEFAULT_CONTEXT}
         super().__init__(
@@ -87,7 +72,22 @@ class CARLJaxPendulumEnv(CARLEnv):
         )  # allow to augment all values
 
     def _update_context(self) -> None:
-        self.env_params = EnvParams(**self.context)
-
-        high = jnp.array([1.0, 1.0, self.max_speed], dtype=jnp.float32)
-        self.build_observation_space(-high, high, CONTEXT_BOUNDS)
+        self.env: CartPole
+        self.env.gravity = self.context["gravity"]
+        self.env.masscart = self.context["masscart"]
+        self.env.masspole = self.context["masspole"]
+        self.env.length = self.context["length"]
+        self.env.force_mag = self.context["force_mag"]
+        self.env.tau = self.context["tau"]
+        
+        high = jnp.array(
+            [
+                self.env.x_threshold * 2,
+                jnp.finfo(jnp.float32).max,
+                self.env.theta_threshold_radians * 2,
+                jnp.finfo(jnp.float32).max,
+            ],
+            dtype=jnp.float32,
+        )
+        low = -high
+        self.build_observation_space(low, high, CONTEXT_BOUNDS)
