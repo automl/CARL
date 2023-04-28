6e31f8bca72beeeea5ae41ce23b4d5907521bdde
Arman717 <125295315+Arman717@users.noreply.github.com>
2023-04-28 19:15:39 +0200

    Delete build/lib/carl directory

--- build/lib/carl/utils/types.py
+++
@@ -1,8 +0,0 @@
-from typing import Any, Dict, List, TypeVar, Union
-
-import numpy as np
-
-Context = Dict[str, Any]
-Contexts = Dict[Any, Context]
-Vector = Union[List[Any], np.ndarray]
-ObsType = TypeVar("ObsType")
