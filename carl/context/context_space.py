47cea9cccc575c1c3308c843a94e2b4094ef6ab7
Jannis Becktepe <jannis@becktepe.de>
2023-05-25 11:45:26 +0200

    added ConfigSpace default values and gym_space for ContextSpace

--- carl/context/context_space.py
+++ carl/context/context_space.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from ConfigSpace.configuration_space import ConfigurationSpace
 from ConfigSpace.hyperparameters import Hyperparameter, UniformFloatHyperparameter
+import gymnasium.spaces as spaces
 
 
 class ContextSpace(ConfigurationSpace):
@@ -14,14 +15,14 @@ class ContextSpace(ConfigurationSpace):
         super().__init__(*args, **kwargs)
 
         self.__context_features: List[str] = []
-        self.__context_space: List[Tuple[Hyperparameter, Union[int, float]]] = []
+        self.__context_space: List[Hyperparameter] = []
         self.__default_context: Dict[str, Union[int, float]] = {}
 
     def __build_context(self):
         """
         Initializes the context based on its context space and features.
         """
-        for parameter, default_value in self.__context_space:
+        for parameter in self.__context_space:
             if parameter.name in self.__context_features:
                 # add context feature as hyperparameter to enable sampling
                 self.add_hyperparameter(parameter)
@@ -29,9 +30,26 @@ class ContextSpace(ConfigurationSpace):
                 # set lower = upper = default to simulate non-varying context features
                 self.add_hyperparameter(UniformFloatHyperparameter(
                     parameter.name,
-                    lower=default_value,
-                    upper=default_value
+                    lower=parameter.default_value,
+                    upper=parameter.default_value
                 ))
+
+    @property
+    def gym_space(self, as_dict: bool = False) -> spaces.Space:
+        if as_dict:
+            context_space = {}
+
+            for parameter in self.__context_space:
+                context_space[parameter.name] = spaces.Box(
+                    low=parameter.lower,
+                    high=parameter.upper
+                )
+            return spaces.Dict(context_space)
+        else:
+            low = np.array([param.lower for param in self.__context_space])
+            high = np.array([param.upper for param in self.__context_space])
+
+            return spaces.Box(low=low, high=high, dtype=np.float32)
 
     @property
     def context_features(self) -> List[str]:
@@ -79,14 +97,14 @@ class CartPoleContextSpace(ContextSpace):
         # ENVIRONMENT SPECIFIC CONTEXT PARAMETERS AND DEFAULT VALUES
         # -----------------------------------------------------------------------------------------
         self.__context = [
-            (UniformFloatHyperparameter("gravity", lower=0.1, upper=np.inf), 9.8),
-            (UniformFloatHyperparameter("masscart", lower=0.1, upper=10), 1.0),
-            (UniformFloatHyperparameter("masspole", lower=0.01, upper=1), 0.1),
-            (UniformFloatHyperparameter("pole_length", lower=0.05, upper=5), 0.5),
-            (UniformFloatHyperparameter("force_magnifier", lower=1, upper=100), 10.0),
-            (UniformFloatHyperparameter("update_interval", lower=0.002, upper=0.2), 0.02),
-            (UniformFloatHyperparameter("initial_state_lower", lower=-np.inf, upper=np.inf), -0.1),
-            (UniformFloatHyperparameter("initial_state_upper", lower=-np.inf, upper=np.inf), 0.1)
+            UniformFloatHyperparameter("gravity", lower=0.1, upper=np.inf, default_value=9.8),
+            UniformFloatHyperparameter("masscart", lower=0.1, upper=10, default_value=1.0),
+            UniformFloatHyperparameter("masspole", lower=0.01, upper=1, default_value=0.1),
+            UniformFloatHyperparameter("pole_length", lower=0.05, upper=5, default_value=0.5),
+            UniformFloatHyperparameter("force_magnifier", lower=1, upper=100, default_value=10.0),
+            UniformFloatHyperparameter("update_interval", lower=0.002, upper=0.2, default_value=0.02),
+            UniformFloatHyperparameter("initial_state_lower", lower=-np.inf, upper=np.inf, default_value=-0.1),
+            UniformFloatHyperparameter("initial_state_upper", lower=-np.inf, upper=np.inf, default_value=0.1)
         ]
         # -----------------------------------------------------------------------------------------
 
