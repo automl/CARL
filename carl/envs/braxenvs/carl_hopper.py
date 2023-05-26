070fc7f17313ce8ee1aeac930af84fd1ba88179d
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2023-05-26 09:26:17 +0200

    Brax envs inherit from brax super env

--- carl/envs/braxenvs/carl_hopper.py
+++ carl/envs/braxenvs/carl_hopper.py
@@ -10,6 +10,7 @@ from carl.context.selection import AbstractSelector
 from carl.envs.carl_env import CARLEnv
 from carl.utils.trial_logger import TrialLogger
 from carl.utils.types import Context, Contexts
+from carl.envs.braxenvs.carl_brax_env import CARLBraxEnv
 
 DEFAULT_CONTEXT = {
     "stiffness_factor": 1,
@@ -33,59 +34,9 @@ CONTEXT_BOUNDS = {
 
 
 
-class CARLHopper(CARLEnv):
-    def __init__(
-        self,
-        env: Hopper = None,
-        n_envs: int = 1,
-        contexts: Contexts = {},
-        hide_context: bool = True,
-        add_gaussian_noise_to_context: bool = False,
-        gaussian_noise_std_percentage: float = 0.01,
-        logger: Optional[TrialLogger] = None,
-        scale_context_features: str = "no",
-        default_context: Optional[Context] = DEFAULT_CONTEXT,
-        state_context_features: Optional[List[str]] = None,
-        context_mask: Optional[List[str]] = None,
-        dict_observation_space: bool = False,
-        context_selector: Optional[
-            Union[AbstractSelector, type[AbstractSelector]]
-        ] = None,
-        context_selector_kwargs: Optional[Dict] = None,
-        max_episode_length = 1000,
-    ):
-        if env is None:
-            batch_size = None if n_envs == 1 else n_envs
-            env = create("hopper", batch_size=batch_size)
-
-        self.n_envs=n_envs
-        if n_envs == 1:
-            env = GymWrapper(env)
-        else:
-            env = VectorGymWrapper(env, n_envs)
-
-        if not contexts:
-            contexts = {0: DEFAULT_CONTEXT}
-        super().__init__(
-            env=env,
-            n_envs=n_envs,
-            contexts=contexts,
-            hide_context=hide_context,
-            add_gaussian_noise_to_context=add_gaussian_noise_to_context,
-            gaussian_noise_std_percentage=gaussian_noise_std_percentage,
-            logger=logger,
-            scale_context_features=scale_context_features,
-            default_context=default_context,
-            state_context_features=state_context_features,
-            dict_observation_space=dict_observation_space,
-            context_selector=context_selector,
-            context_selector_kwargs=context_selector_kwargs,
-            context_mask=context_mask,
-            max_episode_length=max_episode_length,
-        )
-        self.whitelist_gaussian_noise = list(
-            DEFAULT_CONTEXT.keys()
-        )  # allow to augment all values
+class CARLHopper(CARLBraxEnv):
+    env_name: str = "hopper"
+    DEFAULT_CONTEXT: Context = DEFAULT_CONTEXT
 
     def _update_context(self) -> None:
         self.env: Hopper
@@ -107,9 +58,3 @@ class CARLHopper(CARLEnv):
         geoms[0] = geoms[0].replace(friction=jnp.array([self.context["friction"]]))
         config["geoms"] = geoms
         self.env._env.sys = self.env._env.sys.replace(**config)
-
-    def __getattr__(self, name: str) -> Any:
-        if name in ["sys", "__getstate__"]:
-            return getattr(self.env._environment, name)
-        else:
-            return getattr(self, name)
