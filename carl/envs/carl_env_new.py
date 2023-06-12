c4d951c5eb1e6a1870dbc99635f4c4b4b4884fcd
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2023-06-12 15:47:47 +0200

    Change internal representation of context space

--- carl/envs/carl_env_new.py
+++ carl/envs/carl_env_new.py
@@ -88,7 +88,7 @@ class CARLEnv(Wrapper, abc.ABC):
         return obs_space
 
     @staticmethod
-    def get_context_features() -> list[ContextFeature]:
+    def get_context_features() -> dict[str, ContextFeature]:
         # Child env specific
         raise NotImplementedError
 
@@ -202,26 +202,32 @@ class CARLCartPole(CARLClassicControlEnv):
             setattr(self.env, k, v)
 
     def get_context_features() -> list[ContextFeature]:
-        return [
+        return {
+            "gravity": 
             UniformFloatContextFeature(
                 "gravity", lower=0.1, upper=np.inf, default_value=9.8
             ),
+            "masscart": 
             UniformFloatContextFeature(
                 "masscart", lower=0.1, upper=10, default_value=1.0
             ),
+            "masspole":
             UniformFloatContextFeature(
                 "masspole", lower=0.01, upper=1, default_value=0.1
             ),
+            "length":
             UniformFloatContextFeature(
                 "length", lower=0.05, upper=5, default_value=0.5
             ),
+            "force_mag":
             UniformFloatContextFeature(
                 "force_mag", lower=1, upper=100, default_value=10.0
             ),
+            "tau":
             UniformFloatContextFeature(
                 "tau", lower=0.002, upper=0.2, default_value=0.02
             ),
-        ]
+        }
 
 
 if __name__ == "__main__":
