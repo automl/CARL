3531d1ae5bb4342c73f5287f2c9f729dbea88e46
benjamc <c.benjamins@ai.uni-hannover.de>
2023-07-07 16:00:35 +0200

    Current

--- carl/envs/gymnasium/box2d/carl_vehicle_racing.py
+++ carl/envs/gymnasium/box2d/carl_vehicle_racing.py
@@ -1,13 +1,20 @@
+from __future__ import annotations
+
 from typing import Any, Dict, List, Optional, Tuple, Type, Union
 
 import numpy as np
 import pyglet
-from gymnasium.envs.box2d import CarRacing
+from gymnasium.envs.box2d import CarRacing, lunar_lander
 from gymnasium.envs.box2d.car_dynamics import Car
+from gymnasium.envs.box2d.lunar_lander import LunarLander
+from gymnasium.envs.registration import register
 from pyglet import gl
 
-from carl.context.selection import AbstractSelector
-from carl.envs.carl_env import CARLEnv
+from carl.context.context_space import (
+    ContextFeature,
+    UniformFloatContextFeature,
+    UniformIntegerContextFeature,
+)
 from carl.envs.gymnasium.box2d.parking_garage.bus import AWDBus  # as Car
 from carl.envs.gymnasium.box2d.parking_garage.bus import AWDBusLargeTrailer  # as Car
 from carl.envs.gymnasium.box2d.parking_garage.bus import AWDBusSmallTrailer  # as Car
@@ -41,6 +48,7 @@ from carl.envs.gymnasium.box2d.parking_garage.street_car import (  # as Car
 )
 from carl.envs.gymnasium.box2d.parking_garage.trike import TukTuk  # as Car
 from carl.envs.gymnasium.box2d.parking_garage.trike import TukTukSmallTrailer  # as Car
+from carl.envs.gymnasium.carl_gymnasium_env import CARLGymnasiumEnv
 from carl.utils.trial_logger import TrialLogger
 from carl.utils.types import Context, Contexts, ObsType
 
@@ -91,9 +99,14 @@ CONTEXT_BOUNDS = {
 CATEGORICAL_CONTEXT_FEATURES = ["VEHICLE"]
 
 
-class CustomCarRacingEnv(CarRacing):
-    def __init__(self, vehicle_class: Type[Car] = Car, verbose: bool = True):
-        super().__init__(verbose)
+class CustomCarRacing(CarRacing):
+    def __init__(
+        self,
+        vehicle_class: Type[Car] = Car,
+        verbose: bool = True,
+        render_mode: Optional[str] = None,
+    ):
+        super().__init__(verbose=verbose, render_mode=render_mode)
         self.vehicle_class = vehicle_class
 
     def reset(
@@ -195,64 +208,26 @@ class CustomCarRacingEnv(CarRacing):
         vl.draw(gl.GL_QUADS)
 
 
-class CARLVehicleRacingEnv(CARLEnv):
-    def __init__(
-        self,
-        env: CustomCarRacingEnv = CustomCarRacingEnv(),
-        contexts: Optional[Contexts] = None,
-        hide_context: bool = True,
-        add_gaussian_noise_to_context: bool = False,
-        gaussian_noise_std_percentage: float = 0.01,
-        logger: Optional[TrialLogger] = None,
-        scale_context_features: str = "no",
-        default_context: Optional[Context] = DEFAULT_CONTEXT,
-        state_context_features: Optional[List[str]] = None,
-        context_mask: Optional[List[str]] = None,
-        dict_observation_space: bool = False,
-        context_selector: Optional[
-            Union[AbstractSelector, type[AbstractSelector]]
-        ] = None,
-        context_selector_kwargs: Optional[Dict] = None,
-    ):
-        """
+register(
+    id="CustomCarRacing-v2",
+    entry_point="carl.envs.gymnasium.box2d.carl_vehicle_racing:CustomCarRacing",
+    max_episode_steps=1000,
+    reward_threshold=900,
+)
 
-        Parameters
-        ----------
-        env: gym.Env, optional
-            Defaults to classic control environment mountain car from gym (MountainCarEnv).
-        contexts: List[Dict], optional
-            Different contexts / different environment parameter settings.
-        instance_mode: str, optional
-        """
-        if not hide_context:
-            raise NotImplementedError(
-                "The context is already coded in the pixel state, the context cannot be hidden that easily. "
-                "Due to the pixel state we cannot easily concatenate the context to the state, therefore "
-                "hide_context must be True but at the same time the context is visible via the pixel state."
-            )
 
-        if not contexts:
-            contexts = {0: DEFAULT_CONTEXT}
-        super().__init__(
-            env=env,
-            contexts=contexts,
-            hide_context=hide_context,
-            add_gaussian_noise_to_context=add_gaussian_noise_to_context,
-            gaussian_noise_std_percentage=gaussian_noise_std_percentage,
-            logger=logger,
-            scale_context_features=scale_context_features,
-            default_context=default_context,
-            state_context_features=state_context_features,
-            dict_observation_space=dict_observation_space,
-            context_selector=context_selector,
-            context_selector_kwargs=context_selector_kwargs,
-            context_mask=context_mask,
-        )
-        self.whitelist_gaussian_noise = [
-            k for k in DEFAULT_CONTEXT.keys() if k not in CATEGORICAL_CONTEXT_FEATURES
-        ]
+class CARLVehicleRacing(CARLGymnasiumEnv):
+    env_name: str = "CustomCarRacing-v2"
+
+    @staticmethod
+    def get_context_features() -> dict[str, ContextFeature]:
+        return {
+            "VEHICLE_ID": UniformIntegerContextFeature(
+                "VEHICLE_ID", lower=0, upper=len(PARKING_GARAGE) - 1, default_value=0
+            )  # RaceCar
+        }
 
     def _update_context(self) -> None:
-        self.env: CustomCarRacingEnv
-        vehicle_class_index = self.context["VEHICLE"]
+        self.env: CustomCarRacing
+        vehicle_class_index = self.context["VEHICLE_ID"]
         self.env.vehicle_class = PARKING_GARAGE[vehicle_class_index]
