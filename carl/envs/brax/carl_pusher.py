5352995aef95f6a774e7da0c738006da7ad7cf55
amsks <adityak735@gmail.com>
2023-07-14 14:27:06 +0200

    added new brax envs

--- carl/envs/brax/carl_pusher.py
+++ carl/envs/brax/carl_pusher.py
@@ -2,7 +2,7 @@ from typing import Any, Dict, List, Optional, Union
 
 import numpy as np
 import jax.numpy as jnp
-from brax.envs.half_cheetah import Halfcheetah
+from brax.envs.pusher import Pusher
 from brax.envs import create
 from carl.envs.brax.brax_wrappers import GymWrapper, VectorGymWrapper
 
@@ -15,10 +15,9 @@ from carl.envs.brax.carl_brax_env import CARLBraxEnv
 DEFAULT_CONTEXT = {
     "stiffness_factor": 1,
     "gravity": -9.81,
-    "friction": 0.4,
+    "friction": 0.8,
     "damping_factor": 1,
     "actuator_strength_factor": 1,
-    "torso_mass": 6.25,
     "dt": 0.01
 }
 
@@ -28,27 +27,20 @@ CONTEXT_BOUNDS = {
     "friction": (-np.inf, np.inf, float),
     "damping_factor": (-np.inf, np.inf, float),
     "actuator_strength_factor": (1, np.inf, float),
-    "torso_mass": (0.1, np.inf, float),
     "dt": (0.0001, 0.03, float),
 }
 
 
 
-class CARLHalfcheetah(CARLBraxEnv):
-    env_name: str = "halfcheetah"
+class CARLPusher(CARLBraxEnv):
+    env_name: str = "pusher"
     DEFAULT_CONTEXT: Context = DEFAULT_CONTEXT
 
     def _update_context(self) -> None:
-        self.env: Halfcheetah
+        self.env: Pusher
         config = {}
         config["gravity"] = jnp.array([0, 0, self.context["gravity"]])
         config["dt"] = jnp.array(self.context["dt"])
-        new_mass = self.env._env.sys.link.inertia.mass.at[0].set(self.context["torso_mass"])
-        # TODO: do we want to implement this?
-        #new_com = self.env.sys.link.inertia.transform
-        #new_inertia = self.env.sys.link.inertia.i
-        inertia = self.env._env.sys.link.inertia.replace(mass=new_mass)
-        config["link"] = self.env._env.sys.link.replace(inertia=inertia)
         new_stiffness = self.context["stiffness_factor"]*self.env._env.sys.dof.stiffness
         new_damping = self.context["damping_factor"]*self.env._env.sys.dof.damping
         config["dof"] = self.env._env.sys.dof.replace(stiffness=new_stiffness, damping=new_damping)
@@ -58,4 +50,3 @@ class CARLHalfcheetah(CARLBraxEnv):
         geoms[0] = geoms[0].replace(friction=jnp.array([self.context["friction"]]))
         config["geoms"] = geoms
         self.env._env.sys = self.env._env.sys.replace(**config)
-
