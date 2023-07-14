5e4d44ec62d4b15c928ff60f47b192f4dce4268d
amsks <adityak735@gmail.com>
2023-07-14 14:36:38 +0200

    added all envs to init

--- carl/envs/brax/__init__.py
+++ carl/envs/brax/__init__.py
@@ -3,21 +3,33 @@
 from carl.envs.brax.carl_ant import CONTEXT_BOUNDS as CARLAnt_bounds
 from carl.envs.brax.carl_ant import DEFAULT_CONTEXT as CARLAnt_defaults
 from carl.envs.brax.carl_ant import CARLAnt
-from carl.envs.brax.carl_halfcheetah import CONTEXT_BOUNDS as CARLHalfcheetah_bounds
-from carl.envs.brax.carl_halfcheetah import DEFAULT_CONTEXT as CARLHalfcheetah_defaults
-from carl.envs.brax.carl_halfcheetah import CARLHalfcheetah
-from carl.envs.brax.carl_humanoid import CONTEXT_BOUNDS as CARLHumanoid_bounds
-from carl.envs.brax.carl_humanoid import DEFAULT_CONTEXT as CARLHumanoid_defaults
-from carl.envs.brax.carl_humanoid import CARLHumanoid
-from carl.envs.brax.carl_hopper import CONTEXT_BOUNDS as CARLHopper_bounds
-from carl.envs.brax.carl_hopper import DEFAULT_CONTEXT as CARLHopper_defaults
-from carl.envs.brax.carl_hopper import CARLHopper
-from carl.envs.brax.carl_reacher import CONTEXT_BOUNDS as CARLReacher_bounds
-from carl.envs.brax.carl_reacher import DEFAULT_CONTEXT as CARLReacher_defaults
-from carl.envs.brax.carl_reacher import CARLReacher
-from carl.envs.brax.carl_pusher import CONTEXT_BOUNDS as CARLPusher_bounds
-from carl.envs.brax.carl_pusher import DEFAULT_CONTEXT as CARLPusher_defaults
-from carl.envs.brax.carl_pusher import CARLPusher
 from carl.envs.brax.carl_double_pendulum import CONTEXT_BOUNDS as CARLInvertedDoublePendulum_bounds
 from carl.envs.brax.carl_double_pendulum import DEFAULT_CONTEXT as CARLInvertedDoublePendulum_defaults
 from carl.envs.brax.carl_double_pendulum import CARLInvertedDoublePendulum
+from carl.envs.brax.carl_fetch import CONTEXT_BOUNDS as CARLFetch_bounds
+from carl.envs.brax.carl_fetch import DEFAULT_CONTEXT as CARLFetch_defaults
+from carl.envs.brax.carl_fetch import CARLFetch
+from carl.envs.brax.carl_grasp import CONTEXT_BOUNDS as CARLGrasp_bounds
+from carl.envs.brax.carl_grasp import DEFAULT_CONTEXT as CARLGrasp_defaults
+from carl.envs.brax.carl_grasp import CARLGrasp
+from carl.envs.brax.carl_halfcheetah import CONTEXT_BOUNDS as CARLHalfcheetah_bounds
+from carl.envs.brax.carl_halfcheetah import DEFAULT_CONTEXT as CARLHalfcheetah_defaults
+from carl.envs.brax.carl_halfcheetah import CARLHalfcheetah
+from carl.envs.brax.carl_hopper import CONTEXT_BOUNDS as CARLHopper_bounds
+from carl.envs.brax.carl_hopper import DEFAULT_CONTEXT as CARLHopper_defaults
+from carl.envs.brax.carl_hopper import CARLHopper
+from carl.envs.brax.carl_humanoid import CONTEXT_BOUNDS as CARLHumanoid_bounds
+from carl.envs.brax.carl_humanoid import DEFAULT_CONTEXT as CARLHumanoid_defaults
+from carl.envs.brax.carl_humanoid import CARLHumanoid
+from carl.envs.brax.carl_pusher import CONTEXT_BOUNDS as CARLPusher_bounds
+from carl.envs.brax.carl_pusher import DEFAULT_CONTEXT as CARLPusher_defaults
+from carl.envs.brax.carl_pusher import CARLPusher
+from carl.envs.brax.carl_reacher import CONTEXT_BOUNDS as CARLReacher_bounds
+from carl.envs.brax.carl_reacher import DEFAULT_CONTEXT as CARLReacher_defaults
+from carl.envs.brax.carl_reacher import CARLReacher
+from carl.envs.brax.carl_ur5e import CONTEXT_BOUNDS as CARLUr5e_bounds
+from carl.envs.brax.carl_ur5e import DEFAULT_CONTEXT as CARLUr5e_defaults
+from carl.envs.brax.carl_ur5e import CARLUr5e
+
+
+
