0ea79bca23323946346543e037c21ed972e21e25
Theresa Eimer <t.eimer@ai.uni-hannover.de>
2023-07-20 14:00:23 +0200

    more merging

--- test/test_CARLEnv.py
+++ test/test_CARLEnv.py
@@ -8,134 +8,9 @@ CARLPendulum.render_mode = "rgb_array"
 
 
 class TestStateConstruction(unittest.TestCase):
-    def test_hiddenstate(self):
-        """
-        Test if we set hide_context = True that we get the original, normal state.
-        """
-        env = CARLPendulumEnv(
-            contexts={},
-            hide_context=True,
-            add_gaussian_noise_to_context=False,
-            gaussian_noise_std_percentage=0.01,
-            state_context_features=None,
-        )
-        env.reset()
-        action = [0.01]  # torque
-        state, reward, terminated, truncated, info = env.step(action=action)
-        env.close()
-        self.assertEqual(3, len(state))
-
-    def test_visiblestate(self):
-        """
-        Test if we set hide_context = False and state_context_features=None that we get the normal state extended by
-        all context features.
-        """
-        env = CARLPendulumEnv(
-            contexts={},
-            hide_context=False,
-            add_gaussian_noise_to_context=False,
-            gaussian_noise_std_percentage=0.01,
-            state_context_features=None,
-        )
-        env.reset()
-        action = [0.01]  # torque
-        state, reward, terminated, truncated, info = env.step(action=action)
-        env.close()
-        self.assertEqual(10, len(state))
-
-    def test_visiblestate_customnone(self):
-        """
-        Test if we set hide_context = False and state_context_features="changing_context_features" that we get the
-        normal state, not extended by context features.
-        """
-        env = CARLPendulumEnv(
-            contexts={},
-            hide_context=False,
-            add_gaussian_noise_to_context=False,
-            gaussian_noise_std_percentage=0.01,
-            state_context_features=["changing_context_features"],
-        )
-        env.reset()
-        action = [0.01]  # torque
-        state, reward, terminated, truncated, info = env.step(action=action)
-        env.close()
-        # Because we don't change any context features the state length should be 3
-        self.assertEqual(3, len(state))
-
-    def test_visiblestate_custom(self):
-        """
-        Test if we set hide_context = False and state_context_features=["g", "m"] that we get the
-        normal state, extended by the context feature values of g and m.
-        """
-        env = CARLPendulumEnv(
-            contexts={},
-            hide_context=False,
-            add_gaussian_noise_to_context=False,
-            gaussian_noise_std_percentage=0.01,
-            state_context_features=["g", "m"],
-        )
-        env.reset()
-        action = [0.01]  # torque
-        state, reward, terminated, truncated, info = env.step(action=action)
-        env.close()
-        # state should be of length 5 because we add two context features
-        self.assertEqual(5, len(state))
-
-    def test_visiblestate_changingcontextfeatures_nochange(self):
-        """
-        Test if we set hide_context = False and state_context_features="changing_context_features" that we get the
-        normal state, extended by the context features which are changing in the set of contexts. Here: None are
-        changing.
-        """
-        contexts = {
-            "0": {"max_speed": 8.0, "dt": 0.05, "g": 10.0, "m": 1.0, "l": 1.0},
-            "1": {"max_speed": 8.0, "dt": 0.05, "g": 10.0, "m": 1.0, "l": 1.0},
-            "2": {"max_speed": 8.0, "dt": 0.05, "g": 10.0, "m": 1.0, "l": 1.0},
-            "3": {"max_speed": 8.0, "dt": 0.05, "g": 10.0, "m": 1.0, "l": 1.0},
-        }
-        env = CARLPendulumEnv(
-            contexts=contexts,
-            hide_context=False,
-            add_gaussian_noise_to_context=False,
-            gaussian_noise_std_percentage=0.01,
-            state_context_features=["changing_context_features"],
-        )
-        env.reset()
-        action = [0.01]  # torque
-        state, reward, terminated, truncated, info = env.step(action=action)
-        env.close()
-        # state should be of length 3 because all contexts are the same
-        self.assertEqual(3, len(state))
-
-    def test_visiblestate_changingcontextfeatures_change(self):
-        """
-        Test if we set hide_context = False and state_context_features="changing_context_features" that we get the
-        normal state, extended by the context features which are changing in the set of contexts.
-        Here: Two are changing.
-        """
-        contexts = {
-            "0": {"max_speed": 8.0, "dt": 0.03, "g": 10.0, "m": 1.0, "l": 1.0},
-            "1": {"max_speed": 8.0, "dt": 0.05, "g": 10.0, "m": 1.0, "l": 0.95},
-            "2": {"max_speed": 8.0, "dt": 0.05, "g": 10.0, "m": 1.0, "l": 0.3},
-            "3": {"max_speed": 8.0, "dt": 0.05, "g": 10.0, "m": 1.0, "l": 1.3},
-        }
-        env = CARLPendulumEnv(
-            contexts=contexts,
-            hide_context=False,
-            add_gaussian_noise_to_context=False,
-            gaussian_noise_std_percentage=0.01,
-            state_context_features=["changing_context_features"],
-        )
-        env.reset()
-        action = [0.01]  # torque
-        state, reward, terminated, truncated, info = env.step(action=action)
-        env.close()
-        # state should be of length 5 because two features are changing (dt and l)
-        self.assertEqual(5, len(state))
-
     def test_dict_observation_space(self):
         contexts = {"0": {"max_speed": 8.0, "dt": 0.03, "g": 10.0, "m": 1.0, "l": 1.0}}
-        env = CARLPendulumEnv(
+        env = CARLPendulum(
             contexts=contexts,
             hide_context=False,
             dict_observation_space=True,
@@ -143,7 +18,7 @@ class TestStateConstruction(unittest.TestCase):
             gaussian_noise_std_percentage=0.01,
             state_context_features=["changing_context_features"],
         )
-        obs = env.reset()
+        obs, _ = env.reset()
         self.assertEqual(type(obs), dict)
         self.assertTrue("state" in obs)
         self.assertTrue("context" in obs)
