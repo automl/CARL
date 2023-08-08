252494d6c17763c15265b00e3710035177424e4c
benjamc <c.benjamins@ai.uni-hannover.de>
2023-08-08 11:57:33 +0200

    Those files sneaked in

--- carl/envs/brax/carl_fast.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from carl.envs.brax.carl_brax_env import CARLBraxEnv
-
-
-class CARLBraxFast(CARLBraxEnv):
-    env_name: str = "fast"
-    asset_path: str = "envs/assets/fast.xml"
