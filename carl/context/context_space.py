3a84089c254f0a376ebbe2ee922dbbe25f003dbd
benjamc <c.benjamins@ai.uni-hannover.de>
2023-08-09 13:59:43 +0200

    Add docstrings

--- carl/context/context_space.py
+++ carl/context/context_space.py
@@ -26,6 +26,13 @@ CategoricalContextFeature: TypeAlias = CategoricalHyperparameter
 
 class ContextSpace(object):
     def __init__(self, context_space: dict[str, ContextFeature]) -> None:
+        """Context space
+
+        Parameters
+        ----------
+        context_space : dict[str, ContextFeature]
+            Raw definition of the context space.
+        """
         self.context_space = context_space
 
     @property
@@ -43,6 +50,20 @@ class ContextSpace(object):
     def insert_defaults(
         self, context: Context, context_keys: List[str] | None = None
     ) -> Context:
+        """Insert default context if keys missing. 
+
+        Parameters
+        ----------
+        context : Context
+            The context.
+        context_keys : List[str] | None, optional
+            Insert defaults only for certain keys, by default None.
+
+        Returns
+        -------
+        Context
+            The filled context with default values.
+        """
         context_with_defaults = self.get_default_context()
 
         # insert defaults only for certain keys
@@ -55,6 +76,21 @@ class ContextSpace(object):
         return context_with_defaults
 
     def verify_context(self, context: Context) -> bool:
+        """Verify context. 
+
+        Check if context feature names are correct and the 
+        values are in bounds.
+
+        Parameters
+        ----------
+        context : Context
+            The context to check.
+
+        Returns
+        -------
+        bool
+            True if valid, False if not.
+        """
         is_valid = True
         cfs = self.context_feature_names
         for cfname, v in context.items():
@@ -73,12 +109,31 @@ class ContextSpace(object):
         return is_valid
 
     def get_default_context(self) -> Context:
+        """Get the default context from the context space.
+
+        Returns
+        -------
+        Context
+            Default context.
+        """
         context = {cf.name: cf.default_value for cf in self.context_space.values()}
         return context
 
     def get_lower_and_upper_bound(
         self, context_feature_name: str
     ) -> tuple[float, float]:
+        """Get lower and upper bounds for each context feature from the context space.
+
+        Parameters
+        ----------
+        context_feature_name : str
+            Name of context feature to get the bounds for.
+
+        Returns
+        -------
+        tuple[float, float]
+            Lower and upper bound as a tuple.
+        """
         cf = self.context_space[context_feature_name]
         bounds = (cf.lower, cf.upper)
         return bounds
@@ -86,6 +141,22 @@ class ContextSpace(object):
     def to_gymnasium_space(
         self, context_feature_names: List[str] | None = None, as_dict: bool = False
     ) -> spaces.Space:
+        """Convert the context space to a gymnasium space (box).
+
+        Parameters
+        ----------
+        context_feature_names : List[str] | None, optional
+            The context features that should be included in the space, by default None.
+            If it is None, then use all available context features.
+        as_dict : bool, optional
+            If True, create a dict gymnasium space, by default False. If False,
+            context feature values as a vector.
+
+        Returns
+        -------
+        spaces.Space
+            Gymnasium space which can be used as an observation space.
+        """
         if context_feature_names is None:
             context_feature_names = self.context_feature_names
         if as_dict:
@@ -115,6 +186,26 @@ class ContextSpace(object):
     def sample_contexts(
         self, context_keys: List[str] | None = None, size: int = 1
     ) -> Context | List[Contexts]:
+        """Sample a number of contexts from the space.
+
+        Parameters
+        ----------
+        context_keys : List[str] | None, optional
+            The context feature names to sample for, by default None
+        size : int, optional
+            The number of contexts to sample, by default 1
+
+        Returns
+        -------
+        Context | List[Contexts]
+            A context or list of contexts. Always filled with defaults
+            if context features missing.
+
+        Raises
+        ------
+        ValueError
+            When elements of context_keys are not valid.
+        """
         if context_keys is None:
             context_keys = self.context_space.keys()
         else:
