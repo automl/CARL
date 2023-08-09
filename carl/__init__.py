d9e3fbadc23198783521244d3e127389bd0f47e0
Theresa Eimer <t.eimer@ai.uni-hannover.de>
2023-08-09 12:13:52 +0200

    version upgrade

--- carl/__init__.py
+++ carl/__init__.py
@@ -1,5 +1,5 @@
 __license__ = "Apache-2.0 License"
-__version__ = "0.2.0"
+__version__ = "1.0.0"
 __author__ = "Carolin Benjamins, Theresa Eimer, Frederik Schubert, André Biedenkapp, Aditya Mohan, Sebastian Döhler"
 
 
