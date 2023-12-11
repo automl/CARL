a35e087e702f0c138eaa2461be863a66fbc3d469
benjamc <c.benjamins@ai.uni-hannover.de>
2023-12-11 13:40:03 +0100

    Fix context space

--- carl/envs/gymnax/classic_control/carl_gymnax_mountaincar.py
+++ carl/envs/gymnax/classic_control/carl_gymnax_mountaincar.py
@@ -14,12 +14,31 @@ class CARLGymnaxMountainCar(CARLGymnaxEnv):
     @staticmethod
     def get_context_features() -> dict[str, ContextFeature]:
         return {
-            "min_position": UniformFloatContextFeature(
-                "min_position", lower=-np.inf, upper=np.inf, default_value=-1.2
+            "max_speed": UniformFloatContextFeature(
+                "max_speed", lower=1e-3, upper=10, default_value=0.07
             ),
-            "max_position": UniformFloatContextFeature(
-                "max_position", lower=-np.inf, upper=np.inf, default_value=0.6
+            "goal_position": UniformFloatContextFeature(
+                "goal_position", lower=-2, upper=2, default_value=0.45
             ),
+            "goal_velocity": UniformFloatContextFeature(
+                "goal_velocity", lower=-10, upper=10, default_value=0
+            ),
+            "force": UniformFloatContextFeature(
+                "force", lower=-10, upper=10, default_value=0.001
+            ),
+            "gravity": UniformFloatContextFeature(
+                "gravity", lower=10, upper=-10, default_value=0.0025
+            ),
+        }
+
+
+class CARLGymnaxMountainCarContinuous(CARLGymnaxMountainCar):
+    env_name: str = "MountainCarContinuous-v0"
+    module: str = "classic_control.continuous_mountain_car"
+
+    @staticmethod
+    def get_context_features() -> dict[str, ContextFeature]:
+        return {
             "max_speed": UniformFloatContextFeature(
                 "max_speed", lower=0, upper=np.inf, default_value=0.07
             ),
@@ -29,15 +48,10 @@ class CARLGymnaxMountainCar(CARLGymnaxEnv):
             "goal_velocity": UniformFloatContextFeature(
                 "goal_velocity", lower=-np.inf, upper=np.inf, default_value=0
             ),
-            "force": UniformFloatContextFeature(
-                "force", lower=-np.inf, upper=np.inf, default_value=0.001
+            "power": UniformFloatContextFeature(
+                "power", lower=1e-6, upper=10, default_value=0.001
             ),
             "gravity": UniformFloatContextFeature(
-                "gravity", lower=0, upper=np.inf, default_value=0.0025
+                "gravity", lower=-10, upper=10, default_value=0.0025
             ),
         }
-
-
-class CARLGymnaxMountainCarContinuous(CARLGymnaxMountainCar):
-    env_name: str = "MountainCarContinuous-v0"
-    module: str = "classic_control.continuous_mountain_car"
