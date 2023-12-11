24066169d0db0bc4b8fbc31faff5bb28827dc2ee
benjamc <c.benjamins@ai.uni-hannover.de>
2023-12-11 11:18:45 +0100

    Rename files

--- carl/envs/gymnax/classic_control/__init__.py
+++ carl/envs/gymnax/classic_control/__init__.py
@@ -1,8 +1,8 @@
-from carl.envs.gymnax.classic_control.carl_jax_acrobot import CARLGymnaxAcrobot
-from carl.envs.gymnax.classic_control.carl_jax_cartpole import CARLGymnaxCartPole
-from carl.envs.gymnax.classic_control.carl_jax_mountaincar import CARLGymnaxMountainCar
-from carl.envs.gymnax.classic_control.carl_jax_mountaincar import CARLGymnaxMountainCarContinuous
-from carl.envs.gymnax.classic_control.carl_jax_pendulum import CARLGymnaxPendulum
+from carl.envs.gymnax.classic_control.carl_gymnax_acrobot import CARLGymnaxAcrobot
+from carl.envs.gymnax.classic_control.carl_gymnax_cartpole import CARLGymnaxCartPole
+from carl.envs.gymnax.classic_control.carl_gymnax_mountaincar import CARLGymnaxMountainCar
+from carl.envs.gymnax.classic_control.carl_gymnax_mountaincar import CARLGymnaxMountainCarContinuous
+from carl.envs.gymnax.classic_control.carl_gymnax_pendulum import CARLGymnaxPendulum
 
 __all__ = [
     "CARLGymnaxAcrobot",
