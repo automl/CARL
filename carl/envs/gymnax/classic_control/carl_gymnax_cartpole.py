adde74099de176b5faab1254982fdb802433921c
benjamc <c.benjamins@ai.uni-hannover.de>
2023-12-11 12:24:42 +0100

    Update cartpole

--- carl/envs/gymnax/classic_control/carl_gymnax_cartpole.py
+++ carl/envs/gymnax/classic_control/carl_gymnax_cartpole.py
@@ -1,43 +1,47 @@
 from __future__ import annotations
 
 import gymnax
-import jax.numpy as jnp
+import importlib
+import jax.numpy as np
 
 from carl.envs.gymnax.carl_gymnax_env import CARLGymnaxEnv
-from carl.utils.types import Context
-
-DEFAULT_CONTEXT = {
-    "gravity": 9.8,
-    "masscart": 1.0,
-    "masspole": 0.1,
-    "length": 0.5,
-    "force_mag": 10.0,
-    "tau": 0.02,
-    "polemass_length": None,
-    "total_mass": None,
-    "max_steps_in_episode": 500,
-}
-
-CONTEXT_BOUNDS = {
-    "gravity": (5.0, 15.0, float),
-    "masscart": (0.5, 2.0, float),
-    "masspole": (0.05, 0.2, float),
-    "length": (0.25, 1.0, float),
-    "force_mag": (5.0, 15.0, float),
-    "tau": (0.01, 0.05, float),
-    "polemass_length": (0, jnp.inf, float),
-    "total_mass": (0, jnp.inf, float),
-    "max_steps_in_episode": (1, jnp.inf, int),
-}
+from carl.context.context_space import ContextFeature, UniformFloatContextFeature
 
 
 class CARLGymnaxCartPole(CARLGymnaxEnv):
     env_name: str = "CartPole-v1"
+    module: str = "classic_control.cartpole"
+
+    @staticmethod
+    def get_context_features() -> dict[str, ContextFeature]:
+        return {
+            "gravity": UniformFloatContextFeature(
+                "gravity", lower=0.1, upper=np.inf, default_value=9.8
+            ),
+            "masscart": UniformFloatContextFeature(
+                "masscart", lower=0.1, upper=10, default_value=1.0
+            ),
+            "masspole": UniformFloatContextFeature(
+                "masspole", lower=0.01, upper=1, default_value=0.1
+            ),
+            "length": UniformFloatContextFeature(
+                "length", lower=0.05, upper=5, default_value=0.5
+            ),
+            "force_mag": UniformFloatContextFeature(
+                "force_mag", lower=1, upper=100, default_value=10.0
+            ),
+            "tau": UniformFloatContextFeature(
+                "tau", lower=0.002, upper=0.2, default_value=0.02
+            ),
+        }
 
     def _update_context(self) -> None:
         content = self.env.env_params.__dict__
         content.update(self.context)
-        # We cannot directly set attributes of env_params because it is a frozen dataclass
-        self.env.env.env_params = (
-            gymnax.environments.classic_control.cartpole.EnvParams(**content)
-        )
+        content["total_mass"] = content["masspole"] + content["masscart"]
+        content["polemass_length"] = content["masspole"] * content["length"]
+
+        # TODO Make this faster by preloading module?
+        self.env.env.env_params = getattr(
+            importlib.import_module(f"gymnax.environments.{self.module}"), "EnvParams"
+        )(**content)
