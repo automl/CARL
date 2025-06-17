fadaa0928bf55177723779d6689ea2eced6fc5dd
Theresa Eimer <t.eimer@ai.uni-hannover.de>
2025-06-17 14:26:07 +0200

    update more versions

--- test/test_box2d_envs.py
+++ test/test_box2d_envs.py
@@ -7,20 +7,27 @@ import carl.envs.gymnasium
 
 class TestBox2DEnvs(unittest.TestCase):
     def test_envs(self):
-        envs = inspect.getmembers(carl.envs.gymnasium.box2d)
+        spec = iutil.find_spec("Box2D")
+        found = spec is not None
+        if found:
+            envs = inspect.getmembers(carl.envs.gymnasium.box2d)
 
-        for env_name, env_obj in envs:
-            if inspect.isclass(env_obj) and "CARL" in env_name:
-                try:
-                    env_obj.get_context_features()
+            for env_name, env_obj in envs:
+                if inspect.isclass(env_obj) and "CARL" in env_name:
+                    try:
+                        env_obj.get_context_features()
 
-                    env = env_obj()
-                    env._progress_instance()
-                    env._update_context()
-                    env.reset()
-                except Exception as e:
-                    print(f"Cannot instantiate {env_name} environment.")
-                    raise e
+                        env = env_obj()
+                        env._progress_instance()
+                        env._update_context()
+                        env.reset()
+                    except Exception as e:
+                        print(f"Cannot instantiate {env_name} environment.")
+                        raise e
+        else:
+            print("Box2D not found, skipping tests.")
+
+        
 
 
 if __name__ == "__main__":
